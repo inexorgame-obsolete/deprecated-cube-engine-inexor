966c2c85d102468be8b2139ab3663237e0fe9ed4
Karolin Varner <karo@cupdev.net>
2011-07-11 23:38:56 +0000

    Fix a buggy guggy bugbug
    git-svn-id: http://svn.nooblounge.net/projects/bomberman/trunk@24 756f4d6d-18b5-4d01-b4fd-6730f0f2117f

--- src/fpsgame/server.cpp
+++ src/fpsgame/server.cpp
@@ -2430,7 +2430,7 @@ namespace server
                 exp->millis = cq ? cq->geteventmillis(gamemillis, cmillis) : 0;
                 exp->gun = getint(p);
                 exp->id = getint(p);
-                if(exp->gun==GUN_BOMB) cq->ammo[GUN_BOMB]++; // add a bomb if the bomb explodes
+                if(exp->gun==GUN_BOMB) cq->state.ammo[GUN_BOMB]++; // add a bomb if the bomb explodes
                 int hits = getint(p);
                 loopk(hits)
                 {
