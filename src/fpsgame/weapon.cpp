750947eb352b5ba12bcf68b7b479d7c3ff2fc6bf
Karolin Varner <karo@cupdev.net>
2011-07-15 02:53:27 +0000

    Bombs do not  bouncd / explode on collide.
    git-svn-id: http://svn.nooblounge.net/projects/bomberman/trunk@33 756f4d6d-18b5-4d01-b4fd-6730f0f2117f

--- src/fpsgame/weapon.cpp
+++ src/fpsgame/weapon.cpp
@@ -252,7 +252,7 @@ namespace game
             vec old(bnc.o);
             bool stopped = false;
             if(bnc.bouncetype==BNC_GRENADE) stopped = bounce(&bnc, 0.6f, 0.5f) || (bnc.lifetime -= time)<0;
-			else if(bnc.bouncetype==BNC_BOMB) stopped = bounce(&bnc, 0.4f, 0.92f) || (bnc.lifetime -= time)<0;
+			else if(bnc.bouncetype==BNC_BOMB) stopped = (bnc.lifetime -= time)<0;
             else
             {
                 // cheaper variable rate physics for debris, gibs, etc.
@@ -262,7 +262,7 @@ namespace game
                     rtime -= qtime;
                     if((bnc.lifetime -= qtime)<0 || bounce(&bnc, qtime/1000.0f, 0.6f, 0.5f)) { stopped = true; break; }
                 }
-            }
+			}
             if(stopped)
             {
                 if(bnc.bouncetype==BNC_GRENADE)
