958e6e0f5986cb8bb98ad3b1e3c29052a2c69a82
aschaeffer <andreas@schaeffernet.de>
2011-07-15 18:20:56 +0000

    + added hud icon for bomb radius + added bomb items texture
    git-svn-id: http://svn.nooblounge.net/projects/bomberman/trunk@38 756f4d6d-18b5-4d01-b4fd-6730f0f2117f

--- src/fpsgame/bomb.h
+++ src/fpsgame/bomb.h
@@ -6,8 +6,23 @@ struct bombservmode : servmode
 struct bombclientmode : clientmode
 #endif
 {
+
+	void drawicon(int icon, float x, float y, float sz)
+    {
+		int bicon = icon - HICON_BOMBRADIUS;
+        settexture("packages/hud/bomb_items.png");
+        glBegin(GL_TRIANGLE_STRIP);
+        float tsz = 0.25f, tx = tsz*(bicon%4), ty = tsz*(bicon/4);
+        glTexCoord2f(tx,     ty);     glVertex2f(x,    y);
+        glTexCoord2f(tx+tsz, ty);     glVertex2f(x+sz, y);
+        glTexCoord2f(tx,     ty+tsz); glVertex2f(x,    y+sz);
+        glTexCoord2f(tx+tsz, ty+tsz); glVertex2f(x+sz, y+sz);
+        glEnd();
+    }
+
 	void drawhud(fpsent *d, int w, int h)
 	{
+		// minimap
         glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
         int s = 1800/4, x = 1800*w/h - s - s/10, y = s/10;
         glColor4f(1, 1, 1, minimapalpha);
@@ -19,14 +34,18 @@ struct bombclientmode : clientmode
         float margin = 0.04f, roffset = s*margin, rsize = s + 2*roffset;
         settexture("packages/hud/radar.png", 3);
         drawradar(x - roffset, y - roffset, rsize);
-        #if 0
-        settexture("packages/hud/compass.png", 3);
-        glPushMatrix();
-        glTranslatef(x - roffset + 0.5f*rsize, y - roffset + 0.5f*rsize, 0);
-        glRotatef(camera1->yaw + 180, 0, 0, -1);
-        drawradar(-0.5f*rsize, -0.5f*rsize, rsize);
-        glPopMatrix();
-        #endif
+
+        // bomb radius icon
+        if(d->state == CS_ALIVE)
+        {
+            int x = HICON_X + 3*HICON_STEP + (d->quadmillis ? HICON_SIZE + HICON_SPACE : 0);
+            drawicon(HICON_BOMBRADIUS, x, HICON_Y, HICON_SIZE);
+            glPushMatrix();
+            glScalef(2, 2, 1);
+            draw_textf("%d", (x + HICON_SIZE + HICON_SPACE)/2, HICON_TEXTY/2, d->bombradius);
+            glPopMatrix();
+        }
+
 	}
 };
 extern bombclientmode bombmode;
