b860d16416faef438711d63e080e04063e1ff795
aschaeffer <andreas@schaeffernet.de>
2011-07-16 04:28:05 +0000

    + ellipsecollide fix (extern)
    git-svn-id: http://svn.nooblounge.net/projects/bomberman/trunk@45 756f4d6d-18b5-4d01-b4fd-6730f0f2117f

--- src/shared/iengine.h
+++ src/shared/iengine.h
@@ -270,6 +270,7 @@ extern bool loadents(const char *fname, vector<entity> &ents, uint *crc = NULL);
 // physics
 extern void moveplayer(physent *pl, int moveres, bool local);
 extern bool moveplayer(physent *pl, int moveres, bool local, int curtime);
+extern bool ellipsecollide(physent *d, const vec &dir, const vec &o, const vec &center, float yaw, float xr, float yr, float hi, float lo);
 extern bool collide(physent *d, const vec &dir = vec(0, 0, 0), float cutoff = 0.0f, bool playercol = true);
 extern bool ellipsecollide(physent *d, const vec &dir, const vec &o, const vec &center, float yaw, float xr, float yr, float hi, float lo);
 extern bool bounce(physent *d, float secs, float elasticity, float waterfric);
