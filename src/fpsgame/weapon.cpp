20f30ec2465575a5d189fc22b18b730d00fc499e
aschaeffer <andreas@schaeffernet.de>
2011-07-18 22:49:58 +0000

    + faster explosion animation for bombs + better looking code
    git-svn-id: http://svn.nooblounge.net/projects/bomberman/trunk@68 756f4d6d-18b5-4d01-b4fd-6730f0f2117f

--- src/fpsgame/weapon.cpp
+++ src/fpsgame/weapon.cpp
@@ -457,18 +457,31 @@ namespace game
 
     void explode(bool local, fpsent *owner, const vec &v, dynent *safe, int damage, int gun)
     {
-        particle_splash(PART_SPARK, 200, 300, v, 0xB49B4B, 0.24f * (gun == GUN_BOMB ? owner->bombradius : 1));
-        playsound(S_RLHIT, &v);
-        particle_fireball(v, RL_DAMRAD * (gun == GUN_BOMB ? owner->bombradius : 1), gun!=GUN_GL ? PART_EXPLOSION : PART_EXPLOSION_BLUE, -1, gun!=GUN_GL ? 0xFF8080 : 0x80FFFF, 4.0f * (gun == GUN_BOMB ? owner->bombradius : 1));
-        if(gun==GUN_RL) adddynlight(v, 1.15f*RL_DAMRAD, vec(2, 1.5f, 1), 900, 100, 0, RL_DAMRAD/2, vec(1, 0.75f, 0.5f));
-        else if(gun==GUN_GL) adddynlight(v, 1.15f*RL_DAMRAD, vec(0.5f, 1.5f, 2), 900, 100, 0, 8, vec(0.25f, 1, 1));
-        // TODO: BOMB
-        else if(gun==GUN_BOMB) adddynlight(v, owner->bombradius*RL_DAMRAD, vec(0.5f, 1.5f, 2), 900, 100, 0, 8, vec(1, 1, 0.25f));
-        else adddynlight(v, 1.15f*RL_DAMRAD, vec(2, 1.5f, 1), 900, 100);
-        int numdebris = gun==GUN_BARREL ? rnd(max(maxbarreldebris-5, 1))+5 : rnd(maxdebris-5)+5;
+        int rfactor = gun != GUN_BOMB ? 1 : owner->bombradius,
+            maxsize = RL_DAMRAD * rfactor,
+            size = 4.0f * rfactor,
+            fade = gun!=GUN_BOMB ? -1 : int((maxsize-size)*7), // explosion speed, lower=faster
+            numdebris = gun==GUN_BARREL ? rnd(max(maxbarreldebris-5, 1))+5 : rnd(maxdebris-5)+5;
         vec debrisvel = owner->o==v ? vec(0, 0, 0) : vec(owner->o).sub(v).normalize(), debrisorigin(v);
-        if(gun==GUN_RL) debrisorigin.add(vec(debrisvel).mul(8));
-        if(gun==GUN_BOMB && owner->ammo[GUN_BOMB] < itemstats[11].max) owner->ammo[GUN_BOMB]++; // add a bomb if the bomb explodes // FIXME: index=11
+        particle_splash(PART_SPARK, 200, 300, v, 0xB49B4B, 0.24f*rfactor);
+        playsound(S_RLHIT, &v);
+        particle_fireball(v, maxsize, gun!=GUN_GL ? PART_EXPLOSION : PART_EXPLOSION_BLUE, fade, gun!=GUN_GL ? 0xFF8080 : 0x80FFFF, size);
+        switch(gun)
+        {
+            case GUN_RL:
+            	adddynlight(v, 1.15f*RL_DAMRAD, vec(2, 1.5f, 1), 900, 100, 0, RL_DAMRAD/2, vec(1, 0.75f, 0.5f));
+            	debrisorigin.add(vec(debrisvel).mul(8));
+                break;
+            case GUN_GL:
+            	adddynlight(v, 1.15f*RL_DAMRAD, vec(0.5f, 1.5f, 2), 900, 100, 0, 8, vec(0.25f, 1, 1));
+            	break;
+            case GUN_BOMB:
+            	adddynlight(v, owner->bombradius*RL_DAMRAD, vec(0.5f, 1.5f, 2), 900, 100, 0, 8, vec(1, 1, 0.25f));
+            	if(owner->ammo[GUN_BOMB] < itemstats[11].max) owner->ammo[GUN_BOMB]++; // add a bomb if the bomb explodes // FIXME: index=11
+            	break;
+            default:
+            	adddynlight(v, 1.15f*RL_DAMRAD, vec(2, 1.5f, 1), 900, 100);
+        }
         if(numdebris)
         {
             entitylight light;
@@ -1061,6 +1074,7 @@ namespace game
 	    {
 	    	bouncer *p = bouncers[i];
 	    	if(p->bouncetype != BNC_BOMB) continue;
+	    	conoutf("check collide with bomb bouncer");
 	    	if(!ellipsecollide(d, dir, p->o, p->o, 0, BOMB_COLLBULGE, BOMB_COLLBULGE, BOMB_COLLHEIGHT, BOMB_COLLGROUND)) return false;
 	    }
 		return true;
