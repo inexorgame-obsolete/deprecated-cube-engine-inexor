5b43c9e50e6e90571b18e60d82b86b3e00567a93
aschaeffer <andreas@schaeffernet.de>
2011-07-22 20:44:24 +0000

    TEST ++++ TEST ++++ temporarly remove bomb mode's no damage barrel behaviour ++++ TEST ++++ TEST
    git-svn-id: http://svn.nooblounge.net/projects/bomberman/trunk@90 756f4d6d-18b5-4d01-b4fd-6730f0f2117f

--- src/fpsgame/movable.cpp
+++ src/fpsgame/movable.cpp
@@ -59,8 +59,9 @@ namespace game
         {
             state = CS_DEAD;
             exploding = 0;
-        	if(!m_bomb) game::explode(true, (fpsent *)at, o, this, guns[GUN_BARREL].damage, GUN_BARREL);
-        	else game::explode(true, (fpsent *)at, o, this, 0, GUN_BARREL); // in bomb mode barrels does explode, but does not cause any damage
+            game::explode(true, (fpsent *)at, o, this, guns[GUN_BARREL].damage, GUN_BARREL);
+            //if(!m_bomb) game::explode(true, (fpsent *)at, o, this, guns[GUN_BARREL].damage, GUN_BARREL);
+            //else game::explode(true, (fpsent *)at, o, this, 0, GUN_BARREL); // in bomb mode barrels does explode, but does not cause any damage
         }
  
         void damaged(int damage, fpsent *at, int gun = -1)
