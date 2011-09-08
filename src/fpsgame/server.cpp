b3e1f1bcace814204b8110a121d47069f8905882
aschaeffer <andreas@schaeffernet.de>
2011-09-08 00:51:25 +0000

    + better temporary workaround for overflow error by pisto
    git-svn-id: http://svn.nooblounge.net/projects/bomberman/trunk@322 756f4d6d-18b5-4d01-b4fd-6730f0f2117f

--- src/fpsgame/server.cpp
+++ src/fpsgame/server.cpp
@@ -1065,7 +1065,7 @@ namespace server
             loopi(sizeof(servtypes)/sizeof(int)) if(type == servtypes[i]) return -1;
             if(type < N_EDITENT || type > N_EDITVAR || !m_edit) 
             {
-                if(type != N_POS && ++ci->overflow >= 1000) return -2; // TEMPORARY FIX (suggested by upper): 200 -> 1000
+                if(type != N_POS && type != N_EXPLODE && ++ci->overflow >= 200) return -2; // TEMPORARY FIX (suggested by upper, enhanced by pisto):
             }
         }
         return type;
