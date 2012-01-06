8d663f51c171ddac03860cad112bf37fe509e458
eihrul <eihrul@07af1644-1164-4067-b897-63bf4dc9b748>
2012-01-06 12:18:41 +0000

    clear anim interp info on reset
    git-svn-id: http://svn.code.sf.net/p/sauerbraten/code@3957 07af1644-1164-4067-b897-63bf4dc9b748

--- src/shared/ents.h
+++ src/shared/ents.h
@@ -175,6 +175,8 @@ struct animinterpinfo // used for animation blending of animated characters
     void *lastmodel;
 
     animinterpinfo() : lastswitch(-1), lastmodel(NULL) {}
+
+    void reset() { lastswitch = -1; }
 };
 
 #define MAXANIMPARTS 3
@@ -215,6 +217,7 @@ struct dynent : physent                         // animated characters, or chara
     {
         physent::reset();
         stopmoving();
+        loopi(MAXANIMPARTS) animinterp[i].reset();
     }
 
     vec abovehead() { return vec(o).add(vec(0, 0, aboveeye+4)); }
