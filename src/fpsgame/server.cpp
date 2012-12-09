cb64195ccb94c2378e933724a54abd9d69963b27
eihrul <eihrul@07af1644-1164-4067-b897-63bf4dc9b748>
2012-12-09 18:28:23 +0000

    log chat in dedicated server
    git-svn-id: http://svn.code.sf.net/p/sauerbraten/code@4371 07af1644-1164-4067-b897-63bf4dc9b748

--- src/fpsgame/server.cpp
+++ src/fpsgame/server.cpp
@@ -3032,6 +3032,7 @@ namespace server
                 getstring(text, p);
                 filtertext(text, text);
                 QUEUE_STR(text);
+                if(isdedicatedserver()) logoutf("%s: %s", colorname(cq), text);
                 break;
             }
 
@@ -3045,6 +3046,7 @@ namespace server
                     if(t==cq || t->state.state==CS_SPECTATOR || t->state.aitype != AI_NONE || strcmp(cq->team, t->team)) continue;
                     sendf(t->clientnum, 1, "riis", N_SAYTEAM, cq->clientnum, text);
                 }
+                if(isdedicatedserver()) logoutf("%s<%s>: %s", colorname(cq), cq->team, text);
                 break;
             }
 
