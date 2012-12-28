35385fd85b22c6bf66f0b03b1ca6a48f4261bfd6
eihrul <eihrul@07af1644-1164-4067-b897-63bf4dc9b748>
2012-12-28 02:24:33 +0000

    local checkpausegame fix
    git-svn-id: http://svn.code.sf.net/p/sauerbraten/code@4529 07af1644-1164-4067-b897-63bf4dc9b748

--- src/fpsgame/server.cpp
+++ src/fpsgame/server.cpp
@@ -2556,7 +2556,6 @@ namespace server
         if(ci->connected)
         {
             if(ci->privilege) setmaster(ci, false);
-            else if(ci->local) checkpausegame();
             if(smode) smode->leavegame(ci, true);
             ci->state.timeplayed += lastmillis - ci->state.lasttimeplayed;
             savescore(ci);
@@ -2564,6 +2563,7 @@ namespace server
             clients.removeobj(ci);
             aiman::removeai(ci);
             if(!numclients(-1, false, true)) noclients(); // bans clear when server empties
+            if(ci->local) checkpausegame();
         }
         else connects.removeobj(ci);
     }
