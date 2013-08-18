c2fbae08f69dc250b43c075009f94dfd97f00539
eihrul <eihrul@07af1644-1164-4067-b897-63bf4dc9b748>
2013-08-18 12:42:29 +0000

    recorder::capture fix
    git-svn-id: http://svn.code.sf.net/p/sauerbraten/code@4888 07af1644-1164-4067-b897-63bf4dc9b748

--- src/engine/engine.h
+++ src/engine/engine.h
@@ -655,7 +655,7 @@ extern uchar shouldsaveblendmap();
 namespace recorder
 {
     extern void stop();
-    extern void capture();
+    extern void capture(bool overlay = true);
     extern void cleanup();
 }
 
