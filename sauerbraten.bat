53f1fb2073e03b0b9f5865f7f03b787b6e2292ac
Karolin Varner <karo@cupdev.net>
2013-12-17 17:59:22 +0100

    bat cleanups
    git-svn-id: http://svn.code.sf.net/p/sauerbraten/code@4178 07af1644-1164-4067-b897-63bf4dc9b748
    
    Conflicts:
    
    	sauerbraten.bat

--- sauerbraten.bat
+++ sauerbraten.bat
@@ -1,2 +1,12 @@
-start bin\sauerbraten.exe -glog.txt -r %*
-start bin\sauerbraten.exe "-q$HOME\My Games\Sauerbraten" -glog.txt %*
+@ECHO OFF
+
+set SAUER_BIN=bin
+
+IF /I "%PROCESSOR_ARCHITECTURE%" == "amd64" (
+    set SAUER_BIN=bin64
+)
+IF /I "%PROCESSOR_ARCHITEW6432%" == "amd64" (
+    set SAUER_BIN=bin64
+)
+
+start %SAUER_BIN%\sauerbraten.exe "-q$HOME\My Games\Sauerbraten" -glog.txt %*
