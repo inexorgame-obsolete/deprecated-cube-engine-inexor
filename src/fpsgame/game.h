88728a29d1bdd35c82756231f4b555d83c839830
aschaeffer <andreas@schaeffernet.de>
2014-01-22 20:28:39 +0100

    fixed missing health icon

--- src/fpsgame/game.h
+++ src/fpsgame/game.h
@@ -424,6 +424,7 @@ static struct itemstat { int add, max, sound; const char *name; int icon, info; 
     {5,     15,    S_ITEMAMMO,   "RI", HICON_RIFLE, GUN_RIFLE},
     {10,    30,    S_ITEMAMMO,   "GL", HICON_GL, GUN_GL},
     {30,    120,   S_ITEMAMMO,   "PI", HICON_PISTOL, GUN_PISTOL},
+    {25,    100,   S_ITEMHEALTH, "H",  HICON_HEALTH},
     {10,    1000,  S_ITEMHEALTH, "MH", HICON_HEALTH, -1},
     {100,   100,   S_ITEMARMOUR, "GA", HICON_GREEN_ARMOUR, A_GREEN},
     {200,   200,   S_ITEMARMOUR, "YA", HICON_YELLOW_ARMOUR, A_YELLOW},
