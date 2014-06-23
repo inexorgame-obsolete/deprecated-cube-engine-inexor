d11e7e98c45d756263e57495c3444d6580d09e74
Karolin Varner <karo@cupdev.net>
2014-06-23 02:43:12 +0200

    Support SVN and GIT when pulling deps

--- tools/tools.sh
+++ tools/tools.sh
@@ -1,20 +1,63 @@
 #! /bin/sh
 
+repo_type() {
+  local git svn r=0 e=""
+
+  test -d "$1/".git && git=1
+  test -d "$1/".svn && svn=1
+
+  if test -n "$git" -a -n "$svn"; then
+    r=101
+    e="Confusing. Found both git and svn dir!"
+  elif test -n "$git"; then
+    echo git
+  elif test "$svn"; then
+    echo svn
+  else
+    r=102
+    e="Confusing. Found neither git nor svn dir!"
+  fi
+
+  test "$e" && (echo >&2 "$e -- $1"; return "$r")
+}
+
+repo_up_git() {
+  cd "$1"
+  git remote update && git reset --hard origin/master
+  cd -
+}
+repo_up_svn() {
+  cd "$1"
+  svn up
+  cd -
+}
+
+repo_clone_git() {
+  git clone "$2" "$1" --depth 1
+}
+repo_clone_svn() {
+  svn co "$2" "$1"
+}
+
+repo() {
+  local cmd="$2" path="$3" type="$1"
+  shift; shift; shift
+
+  test "$type" = "xxx" && type="`repo_type "$path"`"
+
+  repo_"$cmd"_"$type" "$path" "$@"
+}
+
 github_fetch() {
-  notouch="$3 exists but is not a SVN repo. Not touching."
-	if cd "$3"; then
-    if test -d .svn; then
-      if test -z "$NOUPGIT"; then
-        svn up
-      fi
-    else
-      echo >&2 "$notouch"
-    fi
+  notouch="$3 exists but is not a repo. Not touching."
+	if test -d "$3"; then
+    test -z "$NOUPGIT" && repo xxx up "$3"
   elif test -e "$3"; then
     echo >&2 "$notouch"
   else
     # Not existing; check out
-		svn co https://github.com/"$2"/trunk "$3"
+    url="https://github.com/$2"
+    repo svn clone "$3" "$url/trunk" || repo git clone "$3" "$url"
   fi
 
 }
