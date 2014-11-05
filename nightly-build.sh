877f53dd214acb16658afe8f78e2a0ee548a8087
a-teammate <madoe3@web.de>
2014-11-05 16:56:00 +0100

    bundle data-repository just for the master, but still deploy otherwise
    name the zip accordingly to the plattform (linux or windows)

--- nightly-build.sh
+++ nightly-build.sh
@@ -3,18 +3,23 @@ set -ev
 
 echo "branch: ${TRAVIS_BRANCH}, Job No: ${TRAVIS_JOB_NUMBER}" #e.g. "branch: master, Job No: 251.4"
 
-if [ "${TRAVIS_BRANCH}" != "master" ]; then 
-    exit 0 #it actually errors but we dont want to display a failure every time we are not in master
-fi
-
 if [[ ${TRAVIS_JOB_NUMBER} != *1 && ${TRAVIS_JOB_NUMBER} != *5 ]]; then #just deploy one build for windows and one for linux
-    exit 0
+    exit 0 #do not display as failure
 fi
 
 cd $TRAVIS_BUILD_DIR
-export BUILD_NAME=$(echo "${TRAVIS_BRANCH}-${TRAVIS_JOB_NUMBER}-${TRAVIS_COMMIT}" | sed 's#/#-#')
-git clone --depth 1 https://github.com/sauerbraten-fork/sauerbraten-fork-data.git nightly
-rm -rf ./nightly/.git*
+
+export BUILD_NAME="$(echo "${TRAVIS_BRANCH}-${TRAVIS_JOB_NUMBER}-${TRAVIS_COMMIT}-" | sed 's#/#-#')-$([[ ${TRAVIS_JOB_NUMBER} != *5 ]] && echo linux || echo windows)"
+#e.g. master-251.5-10240128410510-linux
+
+if [ "${TRAVIS_BRANCH}" == "master" ] 
+then # the master-nightly-build has the data-folder included
+    git clone --depth 1 https://github.com/sauerbraten-fork/sauerbraten-fork-data.git nightly
+    rm -rf ./nightly/.git*
+else
+    mkdir nightly
+fi
+
 cp -r ./bin_* nightly
 cp ./sauerbraten_unix nightly
 cp ./sauerbraten.bat nightly
