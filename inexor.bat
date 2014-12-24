e9775b1771e3774e9fceb070217adfa82598180a
aschaeffer <hanack@nooblounge.net>
2014-12-25 00:48:58 +0100

    startup scripts fix for win32 architecture

--- inexor.bat
+++ inexor.bat
@@ -2,8 +2,8 @@
 
 set INEXOR_BIN=bin
 
-IF /I "%PROCESSOR_ARCHITECTURE%" == "amd64" (
-    set INEXOR_BIN=bin64
+IF /I "%PROCESSOR_ARCHITECTURE%" == "x86" (
+    set INEXOR_BIN=bin32
 )
 IF /I "%PROCESSOR_ARCHITEW6432%" == "amd64" (
     set INEXOR_BIN=bin64
