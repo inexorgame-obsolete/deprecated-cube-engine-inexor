4d8e9951419348a12b4940a6acf89dcf98085bc6
aschaeffer <hanack@nooblounge.net>
2015-01-05 15:54:31 +0100

    Added request context handler to CEF client handler

--- src/ui/cefclienthandler.h
+++ src/ui/cefclienthandler.h
@@ -9,18 +9,20 @@ class InexorCefClientHandler : public CefClient,
                                public CefLifeSpanHandler,
                                public CefLoadHandler,
                                public CefKeyboardHandler,
-                               public CefDisplayHandler
+                               public CefDisplayHandler,
+                               public CefRequestContextHandler
 {
     public:
         InexorCefClientHandler(InexorCefRenderHandler *renderHandler); // : m_renderHandler(renderHandler) { };
         virtual ~InexorCefClientHandler();
 
         // Getters for handlers
-        virtual CefRefPtr<CefRenderHandler> GetRenderHandler() { return m_renderHandler; }
+        virtual CefRefPtr<CefRenderHandler> GetRenderHandler() { return render_handler; }
         virtual CefRefPtr<CefLifeSpanHandler> GetLifeSpanHandler() { return this; }
         virtual CefRefPtr<CefLoadHandler> GetLoadHandler() { return this; }
         virtual CefRefPtr<CefKeyboardHandler> GetKeyboardHandler() { return this; }
         virtual CefRefPtr<CefDisplayHandler> GetDisplayHandler() { return this; }
+        virtual CefRefPtr<CefRequestContextHandler> GetRequestContextHandler() { return this; }
 
         // CefLifeSpanHandler
         void OnAfterCreated(CefRefPtr<CefBrowser> browser);
@@ -33,21 +35,26 @@ class InexorCefClientHandler : public CefClient,
         // CefKeyboardHandler
         bool OnPreKeyEvent(CefRefPtr<CefBrowser> browser, const CefKeyEvent& key_event, CefEventHandle os_event, bool* is_keyboard_shortcut);
         bool OnKeyEvent(CefRefPtr<CefBrowser> browser, const CefKeyEvent& key_event, CefEventHandle os_event);
-        void SendKeyEvent(const CefKeyEvent& event) { m_Browser->GetHost()->SendKeyEvent(event); }
+        void SendKeyEvent(const CefKeyEvent& event) { browser->GetHost()->SendKeyEvent(event); }
 
         // CefDisplayHandler
+        void OnAddressChange(CefRefPtr<CefBrowser> browser, CefRefPtr<CefFrame> frame, const CefString& url);
         void OnStatusMessage(CefRefPtr<CefBrowser> browser, const CefString& value);
         bool OnConsoleMessage(CefRefPtr<CefBrowser> browser, const CefString& message, const CefString& source, int line);
 
-        CefRefPtr<CefBrowser> GetBrowser() { return m_Browser; }
-        bool IsClosing() { return m_bIsClosing; }
+        // CefRequestContextHandler
+        CefRefPtr<CefCookieManager> GetCookieManager() { return cookie_manager; }
+
+        CefRefPtr<CefBrowser> GetBrowser() { return browser; }
+        bool IsClosing() { return is_closing; }
 
     protected:
-        CefRefPtr<CefBrowser> m_Browser;
-        CefRefPtr<CefRenderHandler> m_renderHandler;
-        int m_BrowserId;
-        int m_BrowserCount;
-        bool m_bIsClosing;
+        CefRefPtr<CefBrowser> browser;
+        CefRefPtr<CefRenderHandler> render_handler;
+        CefRefPtr<CefCookieManager> cookie_manager;
+        int browser_id;
+        int browser_count;
+        bool is_closing;
 
         // Include the default reference counting implementation.
         IMPLEMENT_REFCOUNTING(InexorCefClientHandler);
