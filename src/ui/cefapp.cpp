287a46ad250f3f42fb3ffdadbe9bdf01d2f3eccb
aschaeffer <hanack@nooblounge.net>
2015-01-05 14:56:54 +0100

    Refactored window info and browser settings

--- src/ui/cefapp.cpp
+++ src/ui/cefapp.cpp
@@ -8,41 +8,13 @@ extern void logoutf(const char *fmt, ...);
 #include "include/cef_command_line.h"
 
 InexorCefApp::InexorCefApp(std::string url, int x, int y, int width, int height) : url(url) {
-    // The window info
     window_info.x = x;
     window_info.y = y;
     window_info.width = width;
     window_info.height = height;
-    window_info.windowless_rendering_enabled = true;
-    window_info.transparent_painting_enabled = true;
-
-    // The browser settings
-    browser_settings.windowless_frame_rate = 60;
-    browser_settings.file_access_from_file_urls = STATE_ENABLED;
-    browser_settings.plugins = STATE_DISABLED;
-    browser_settings.webgl = STATE_DISABLED;
-    browser_settings.java = STATE_DISABLED;
-    browser_settings.caret_browsing = STATE_ENABLED;
-    browser_settings.javascript = STATE_ENABLED;
 }
 
 InexorCefApp::InexorCefApp(std::string url) : url(url) {
-    // The window info
-    window_info.x = 50;
-    window_info.y = 50;
-    window_info.width = 1800;
-    window_info.height = 900;
-    window_info.windowless_rendering_enabled = true;
-    window_info.transparent_painting_enabled = true;
-
-    // The browser settings
-    browser_settings.windowless_frame_rate = 60;
-    browser_settings.file_access_from_file_urls = STATE_ENABLED;
-    browser_settings.plugins = STATE_DISABLED;
-    browser_settings.webgl = STATE_DISABLED;
-    browser_settings.java = STATE_DISABLED;
-    browser_settings.caret_browsing = STATE_ENABLED;
-    browser_settings.javascript = STATE_ENABLED;
 }
 
 void InexorCefApp::OnContextInitialized() {
