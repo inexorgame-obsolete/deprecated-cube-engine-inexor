8db0b147bc92c9a0e96fed905bb8cd106c68cd56
aschaeffer <hanack@nooblounge.net>
2015-01-05 15:25:05 +0100

    Cleanups

--- src/ui/cefapp.cpp
+++ src/ui/cefapp.cpp
@@ -7,19 +7,21 @@ extern void logoutf(const char *fmt, ...);
 #include "include/cef_browser.h"
 #include "include/cef_command_line.h"
 
-InexorCefApp::InexorCefApp(std::string url, int x, int y, int width, int height) : url(url) {
+InexorCefApp::InexorCefApp(std::string url, int x, int y, int width, int height) : url(url)
+{
     window_info.x = x;
     window_info.y = y;
     window_info.width = width;
     window_info.height = height;
 }
 
-InexorCefApp::InexorCefApp(std::string url) : url(url) {
+InexorCefApp::InexorCefApp(std::string url) : url(url)
+{
 }
 
-void InexorCefApp::OnContextInitialized() {
+void InexorCefApp::OnContextInitialized()
+{
     CEF_REQUIRE_UI_THREAD();
-
     logoutf("init: cef: browser instance");
     render_handler = new InexorCefRenderHandler(true, window_info.x, window_info.y, window_info.width, window_info.height);
     render_handler->Initialize();
