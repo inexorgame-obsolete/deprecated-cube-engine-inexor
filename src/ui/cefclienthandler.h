aca30b8ae1179fec8ac7e89730eed0095826489b
aschaeffer <hanack@nooblounge.net>
2015-01-05 15:15:34 +0100

    Make handlers functional in cef client handler Added display handler to cef client handler

--- src/ui/cefclienthandler.h
+++ src/ui/cefclienthandler.h
@@ -8,23 +8,35 @@
 class InexorCefClientHandler : public CefClient,
                                public CefLifeSpanHandler,
                                public CefLoadHandler,
-                               public CefKeyboardHandler
+                               public CefKeyboardHandler,
+                               public CefDisplayHandler
 {
     public:
         InexorCefClientHandler(InexorCefRenderHandler *renderHandler); // : m_renderHandler(renderHandler) { };
         virtual ~InexorCefClientHandler();
-        virtual CefRefPtr<CefRenderHandler> GetRenderHandler();
 
+        // Getters for handlers
+        virtual CefRefPtr<CefRenderHandler> GetRenderHandler() { return m_renderHandler; }
         virtual CefRefPtr<CefLifeSpanHandler> GetLifeSpanHandler() { return this; }
+        virtual CefRefPtr<CefLoadHandler> GetLoadHandler() { return this; }
+        virtual CefRefPtr<CefKeyboardHandler> GetKeyboardHandler() { return this; }
+        virtual CefRefPtr<CefDisplayHandler> GetDisplayHandler() { return this; }
 
+        // CefLifeSpanHandler
         void OnAfterCreated(CefRefPtr<CefBrowser> browser);
         bool DoClose(CefRefPtr<CefBrowser> browser);
         void OnBeforeClose(CefRefPtr<CefBrowser> browser);
 
+        // CefLoadHandler
         virtual void OnLoadError(CefRefPtr<CefBrowser> browser, CefRefPtr<CefFrame> frame, ErrorCode errorCode, const CefString& errorText, const CefString& failedUrl);
 
+        // CefKeyboardHandler
         bool OnPreKeyEvent(CefRefPtr<CefBrowser> browser, const CefKeyEvent& key_event, CefEventHandle os_event, bool* is_keyboard_shortcut);
         bool OnKeyEvent(CefRefPtr<CefBrowser> browser, const CefKeyEvent& key_event, CefEventHandle os_event);
+
+        // CefDisplayHandler
+        void OnStatusMessage(CefRefPtr<CefBrowser> browser, const CefString& value);
+        bool OnConsoleMessage(CefRefPtr<CefBrowser> browser, const CefString& message, const CefString& source, int line);
 
         CefRefPtr<CefBrowser> GetBrowser() { return m_Browser; }
         bool IsClosing() { return m_bIsClosing; }
