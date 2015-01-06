972966b9e8c4d32452edbbd6ace94e3eb7b6bf2a
aschaeffer <hanack@nooblounge.net>
2015-01-06 17:41:58 +0100

    Temporarly send key events to sauer

--- src/engine/main.cpp
+++ src/engine/main.cpp
@@ -952,8 +952,9 @@ void checkinput()
                         }
                 		cef_app->SendKeyEvent(keyEvent);
                 	} else {
-                        processkey(event.key.keysym.sym, event.key.state==SDL_PRESSED);
+                		// processkey(event.key.keysym.sym, event.key.state==SDL_PRESSED);
                 	}
+                    processkey(event.key.keysym.sym, event.key.state==SDL_PRESSED);
                 }
                 break;
 
