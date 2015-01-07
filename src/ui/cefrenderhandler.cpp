f428a112064d889b13f2173dddfa092b97e4b931
aschaeffer <hanack@nooblounge.net>
2015-01-07 18:05:00 +0100

    Small refactoring in renderhandler

--- src/ui/cefrenderhandler.cpp
+++ src/ui/cefrenderhandler.cpp
@@ -36,13 +36,13 @@
 #endif
 
 InexorCefRenderHandler::InexorCefRenderHandler(bool transparent, int x, int y, int width, int height)
-    : transparent_(transparent),
-      initialized_(false),
-      texture_id_(0),
-      view_x_(x),
-      view_y_(y),
-      view_width_(width),
-      view_height_(height) {
+    : transparent(transparent),
+      initialized(false),
+      texture_id(0),
+      view_x(x),
+      view_y(y),
+      view_width(width),
+      view_height(height) {
 }
 
 InexorCefRenderHandler::~InexorCefRenderHandler() {
@@ -50,7 +50,7 @@ InexorCefRenderHandler::~InexorCefRenderHandler() {
 }
 
 void InexorCefRenderHandler::Initialize() {
-    if (initialized_)
+    if (initialized)
         return;
 
     glHint(GL_POLYGON_SMOOTH_HINT, GL_NICEST); VERIFY_NO_ERROR;
@@ -60,27 +60,27 @@ void InexorCefRenderHandler::Initialize() {
     glPixelStorei(GL_UNPACK_ALIGNMENT, 1); VERIFY_NO_ERROR;
 
     // Create the texture.
-    glGenTextures(1, &texture_id_); VERIFY_NO_ERROR;
-    DCHECK_NE(texture_id_, 0U); VERIFY_NO_ERROR;
+    glGenTextures(1, &texture_id); VERIFY_NO_ERROR;
+    DCHECK_NE(texture_id, 0U); VERIFY_NO_ERROR;
 
-    glBindTexture(GL_TEXTURE_2D, texture_id_); VERIFY_NO_ERROR;
+    glBindTexture(GL_TEXTURE_2D, texture_id); VERIFY_NO_ERROR;
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST); VERIFY_NO_ERROR;
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST); VERIFY_NO_ERROR;
     glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_MODULATE); VERIFY_NO_ERROR;
 
-    initialized_ = true;
+    initialized = true;
 }
 
 void InexorCefRenderHandler::Cleanup() {
-    if (texture_id_ != 0)
-        glDeleteTextures(1, &texture_id_);
+    if (texture_id != 0)
+        glDeleteTextures(1, &texture_id);
 }
 
 void InexorCefRenderHandler::Render() {
-    if (view_width_ == 0 || view_height_ == 0 || !initialized_)
+    if (view_width == 0 || view_height == 0 || !initialized)
         return;
 
-    DCHECK(initialized_);
+    DCHECK(initialized);
 
     struct {
         float tu, tv;
@@ -107,8 +107,8 @@ void InexorCefRenderHandler::Render() {
     glEnable(GL_TEXTURE_2D); VERIFY_NO_ERROR;
 
     // Draw the facets with the texture.
-    DCHECK_NE(texture_id_, 0U); VERIFY_NO_ERROR;
-    glBindTexture(GL_TEXTURE_2D, texture_id_); VERIFY_NO_ERROR;
+    DCHECK_NE(texture_id, 0U); VERIFY_NO_ERROR;
+    glBindTexture(GL_TEXTURE_2D, texture_id); VERIFY_NO_ERROR;
     glInterleavedArrays(GL_T2F_V3F, 0, vertices); VERIFY_NO_ERROR;
     glDrawArrays(GL_QUADS, 0, 4); VERIFY_NO_ERROR;
 
@@ -131,8 +131,8 @@ void InexorCefRenderHandler::OnPopupSize(CefRefPtr<CefBrowser> browser, const Ce
 {
     if (rect.width <= 0 || rect.height <= 0)
         return;
-    original_popup_rect_ = rect;
-    popup_rect_ = GetPopupRectInWebView(original_popup_rect_);
+    original_popup_rect = rect;
+    popup_rect = GetPopupRectInWebView(original_popup_rect);
 }
 
 CefRect InexorCefRenderHandler::GetPopupRectInWebView(const CefRect& original_rect)
@@ -144,10 +144,10 @@ CefRect InexorCefRenderHandler::GetPopupRectInWebView(const CefRect& original_re
     if (rc.y < 0)
         rc.y = 0;
     // if popup goes outside the view, try to reposition origin
-    if (rc.x + rc.width > view_width_)
-        rc.x = view_width_ - rc.width;
-    if (rc.y + rc.height > view_height_)
-        rc.y = view_height_ - rc.height;
+    if (rc.x + rc.width > view_width)
+        rc.x = view_width - rc.width;
+    if (rc.y + rc.height > view_height)
+        rc.y = view_height - rc.height;
     // if x or y became negative, move them to 0 again.
     if (rc.x < 0)
         rc.x = 0;
@@ -158,13 +158,13 @@ CefRect InexorCefRenderHandler::GetPopupRectInWebView(const CefRect& original_re
 
 void InexorCefRenderHandler::ClearPopupRects()
 {
-    popup_rect_.Set(0, 0, 0, 0);
-    original_popup_rect_.Set(0, 0, 0, 0);
+    popup_rect.Set(0, 0, 0, 0);
+    original_popup_rect.Set(0, 0, 0, 0);
 }
 
 bool InexorCefRenderHandler::GetViewRect(CefRefPtr<CefBrowser> browser, CefRect &rect)
 {
-    rect = CefRect(view_x_, view_y_, view_width_, view_height_);
+    rect = CefRect(view_x, view_y, view_width, view_height);
     return true;
 }
 
@@ -174,7 +174,7 @@ void InexorCefRenderHandler::OnPaint(
     const CefRenderHandler::RectList& dirtyRects,
     const void* buffer, int width, int height
 ) {
-    if (!initialized_) {
+    if (!initialized) {
     	if (CefCurrentlyOn(TID_UI))
              Initialize();
     	return;
@@ -186,22 +186,22 @@ void InexorCefRenderHandler::OnPaint(
     // Enable 2D textures.
     glEnable(GL_TEXTURE_2D); VERIFY_NO_ERROR;
 
-    DCHECK_NE(texture_id_, 0U);
-    glBindTexture(GL_TEXTURE_2D, texture_id_); VERIFY_NO_ERROR;
+    DCHECK_NE(texture_id, 0U);
+    glBindTexture(GL_TEXTURE_2D, texture_id); VERIFY_NO_ERROR;
 
     if (type == PET_VIEW) {
-        int old_width = view_width_;
-        int old_height = view_height_;
-        view_width_ = width;
-        view_height_ = height;
-        glPixelStorei(GL_UNPACK_ROW_LENGTH, view_width_); VERIFY_NO_ERROR;
-        if (old_width != view_width_ || old_height != view_height_
-            || (dirtyRects.size() == 1 && dirtyRects[0] == CefRect(0, 0, view_width_, view_height_))
+        int old_width = view_width;
+        int old_height = view_height;
+        view_width = width;
+        view_height = height;
+        glPixelStorei(GL_UNPACK_ROW_LENGTH, view_width); VERIFY_NO_ERROR;
+        if (old_width != view_width || old_height != view_height
+            || (dirtyRects.size() == 1 && dirtyRects[0] == CefRect(0, 0, view_width, view_height))
         ) {
             // Update/resize the whole texture.
         	glPixelStorei(GL_UNPACK_SKIP_PIXELS, 0); VERIFY_NO_ERROR;
         	glPixelStorei(GL_UNPACK_SKIP_ROWS, 0); VERIFY_NO_ERROR;
-        	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, view_width_, view_height_, 0, GL_BGRA, GL_UNSIGNED_INT_8_8_8_8_REV, buffer); VERIFY_NO_ERROR;
+        	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, view_width, view_height, 0, GL_BGRA, GL_UNSIGNED_INT_8_8_8_8_REV, buffer); VERIFY_NO_ERROR;
         } else {
             // Update just the dirty rectangles.
             CefRenderHandler::RectList::const_iterator i = dirtyRects.begin();
@@ -215,12 +215,9 @@ void InexorCefRenderHandler::OnPaint(
             }
         }
         glPixelStorei(GL_UNPACK_ROW_LENGTH, 0); VERIFY_NO_ERROR;
-    }
-    /*
-    else if (type == PET_POPUP && popup_rect_.width > 0 && popup_rect_.height > 0) {
-    	logoutf("PET_POPUP");
-        int skip_pixels = 0, x = popup_rect_.x;
-        int skip_rows = 0, y = popup_rect_.y;
+    } else if (type == PET_POPUP && popup_rect.width > 0 && popup_rect.height > 0) {
+        int skip_pixels = 0, x = popup_rect.x;
+        int skip_rows = 0, y = popup_rect.y;
         int w = width;
         int h = height;
 
@@ -233,19 +230,18 @@ void InexorCefRenderHandler::OnPaint(
             skip_rows = -y;
             y = 0;
         }
-        if (x + w > view_width_)
-            w -= x + w - view_width_;
-        if (y + h > view_height_)
-            h -= y + h - view_height_;
+        if (x + w > view_width)
+            w -= x + w - view_width;
+        if (y + h > view_height)
+            h -= y + h - view_height;
 
         // Update the popup rectangle.
         glPixelStorei(GL_UNPACK_ROW_LENGTH, width); VERIFY_NO_ERROR;
         glPixelStorei(GL_UNPACK_SKIP_PIXELS, skip_pixels); VERIFY_NO_ERROR;
         glPixelStorei(GL_UNPACK_SKIP_ROWS, skip_rows); VERIFY_NO_ERROR;
-        glTexSubImage2D(GL_TEXTURE_2D, 0, x, y, w, h, GL_BGRA,
-            GL_UNSIGNED_INT_8_8_8_8_REV, buffer); VERIFY_NO_ERROR;
+        glTexSubImage2D(GL_TEXTURE_2D, 0, x, y, w, h, GL_BGRA, GL_UNSIGNED_INT_8_8_8_8_REV, buffer); VERIFY_NO_ERROR;
+        glPixelStorei(GL_UNPACK_ROW_LENGTH, 0); VERIFY_NO_ERROR;
     }
-    */
 
     // Disable 2D textures.
     glDisable(GL_TEXTURE_2D); VERIFY_NO_ERROR;
