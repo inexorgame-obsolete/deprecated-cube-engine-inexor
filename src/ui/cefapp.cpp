70f12c11bd950e0c442019120a737070e230912f
aschaeffer <hanack@nooblounge.net>
2015-01-08 21:06:51 +0100

    Added event system, added more bindings, added JS debugging

--- src/ui/cefapp.cpp
+++ src/ui/cefapp.cpp
@@ -2,102 +2,55 @@
 
 InexorCefApp::InexorCefApp(int width, int height)
 {
+    mouse = new InexorCefMouse();
+    layer_manager = new InexorCefLayerManager();
+    context = new InexorCefContext(layer_manager);
     SetScreenSize(width, height);
-    mouse.Show();
+    mouse->Show();
 }
 
-InexorCefLayer* InexorCefApp::CreateLayer(std::string name, int x, int y, int width, int height, std::string url)
+void InexorCefApp::Render()
 {
-	InexorCefLayer* layer = new InexorCefLayer(name, x, y, width, height, url);
-	layers.push_back(layer);
-	return layer;
-}
-
-InexorCefLayer* InexorCefApp::GetLayer(std::string name)
-{
-    return NULL;
+    layer_manager->Render();
+    mouse->Render();
 }
 
 void InexorCefApp::SetScreenSize(int width, int height) {
     this->width = width;
     this->height = height;
-    mouse.SetMax(width, height);
-}
-
-void InexorCefApp::RenderLayer(std::string name)
-{
-	GetLayer(name)->GetRenderHandler()->Render();
-}
-
-void InexorCefApp::Render()
-{
-	CEF_REQUIRE_UI_THREAD();
-    for(std::list<InexorCefLayer*>::iterator it = layers.begin(); it != layers.end(); ++it)
-    {
-    	InexorCefLayer* layer = (*it);
-    	if (layer->IsVisible()) {
-        	layer->GetRenderHandler()->Render();
-    	}
-    }
-    // Render mouse in front
-    mouse.Render();
+    mouse->SetMax(width, height);
 }
 
 void InexorCefApp::SendKeyEvent(CefKeyEvent event)
 {
-    for(std::list<InexorCefLayer*>::iterator it = layers.begin(); it != layers.end(); ++it)
-    {
-        InexorCefLayer* layer = (*it);
-        if (layer->IsVisible() && layer->IsAcceptingInput()) {
-            layer->GetBrowser()->GetHost()->SendKeyEvent(event);
-        }
-    }
+    layer_manager->SendKeyEvent(event);
 }
 
 void InexorCefApp::SendMouseClickEvent(const CefMouseEvent& event, CefBrowserHost::MouseButtonType type, bool mouseUp, int clickCount)
 {
-    for(std::list<InexorCefLayer*>::iterator it = layers.begin(); it != layers.end(); ++it)
-    {
-        InexorCefLayer* layer = (*it);
-        if (layer->IsVisible() && layer->IsAcceptingInput()) {
-            layer->GetBrowser()->GetHost()->SendMouseClickEvent(event, type, mouseUp, clickCount);
-        }
-    }
+    layer_manager->SendMouseClickEvent(event, type, mouseUp, clickCount);
 }
 
 void InexorCefApp::SendMouseMoveEvent(const CefMouseEvent& event, bool mouseLeave)
 {
-    for(std::list<InexorCefLayer*>::iterator it = layers.begin(); it != layers.end(); ++it)
-    {
-        InexorCefLayer* layer = (*it);
-        if (layer->IsVisible() && layer->IsAcceptingInput()) {
-            layer->GetBrowser()->GetHost()->SendMouseMoveEvent(event, mouseLeave);
-        }
-    }
-    // Set mouse pointer position
-    mouse.Update(event);
+    layer_manager->SendMouseMoveEvent(event, mouseLeave);
+    mouse->Update(event);
 }
 
 void InexorCefApp::SendMouseWheelEvent(const CefMouseEvent& event, int deltaX, int deltaY)
 {
-    for(std::list<InexorCefLayer*>::iterator it = layers.begin(); it != layers.end(); ++it)
-    {
-        InexorCefLayer* layer = (*it);
-        if (layer->IsVisible() && layer->IsAcceptingInput()) {
-            layer->GetBrowser()->GetHost()->SendMouseWheelEvent(event, deltaX, deltaY);
-        }
-    }
+    layer_manager->SendMouseWheelEvent(event, deltaX, deltaY);
 }
 
 void InexorCefApp::OnContextInitialized()
 {
     CEF_REQUIRE_UI_THREAD();
-    logoutf("init: cef: create layers");
-    InexorCefLayer* layer2 = CreateLayer("menu-navigation", 0, 0, width, height, "http://gitdemo.inexor.org/menuprototype/");
+    // cefdebug("InexorCefApp::OnContextInitialized", "Create Layers");
+    InexorCefLayer* layer2 = layer_manager->CreateLayer("menu-navigation", 0, 0, width, height, "http://gitdemo.inexor.org/menuprototype/");
     layer2->SetVisibility(true);
     layer2->SetIsAcceptingInput(true);
     layer2->SetFocus(true);
-    InexorCefLayer* layer3 = CreateLayer("inexor-object", 0, 0, width, height, "http://gitdemo.inexor.org/inexor-object/");
+    InexorCefLayer* layer3 = layer_manager->CreateLayer("inexor-object", 0, 0, width, height, "http://gitdemo.inexor.org/inexor-object/");
     layer3->SetVisibility(true);
     layer3->SetIsAcceptingInput(true);
     layer3->SetFocus(true);
@@ -105,7 +58,7 @@ void InexorCefApp::OnContextInitialized()
 
 void InexorCefApp::OnContextCreated(CefRefPtr<CefBrowser> browser, CefRefPtr<CefFrame> frame, CefRefPtr<CefV8Context> browser_context)
 {
-    logoutf("Injecting inexor object into javascript context");
+    // cefdebug("InexorCefApp::OnContextCreated", "Injecting inexor object into javascript context");
     // only inject context if...
-    browser_context->GetGlobal()->SetValue("inexor", context.GetContext(), V8_PROPERTY_ATTRIBUTE_NONE);
+    browser_context->GetGlobal()->SetValue("inexor", context->GetContext(), V8_PROPERTY_ATTRIBUTE_NONE);
 }
