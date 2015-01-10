228985f61d25f460207907583e44556a7cfbde69
aschaeffer <hanack@nooblounge.net>
2015-01-10 20:48:57 +0100

    Menu layer: show / hide

--- src/ui/cefmenu.cpp
+++ src/ui/cefmenu.cpp
@@ -2,17 +2,31 @@
 
 void InexorCefMenu::InitializeContext()
 {
+    CreateFunction("show", this);
+    CreateFunction("hide", this);
+    CreateVariable("visible", true);
 }
 
 bool InexorCefMenu::Execute(const CefString& name, CefRefPtr<CefV8Value> object, const CefV8ValueList& arguments, CefRefPtr<CefV8Value>& retval, CefString& exception)
 {
     CEF_REQUIRE_RENDERER_THREAD();
+    if (name == "show") {
+        Show();
+        return true;
+    } else if (name == "hide") {
+        Hide();
+        return true;
+    }
     return false;
 }
 
 bool InexorCefMenu::Get(const CefString& name, const CefRefPtr<CefV8Value> object, CefRefPtr<CefV8Value>& return_value, CefString& exception)
 {
     CEF_REQUIRE_RENDERER_THREAD();
+    if (name == "visible") {
+        return_value = CefV8Value::CreateBool(is_visible);
+        return true;
+    }
     return false;
 }
 
@@ -22,3 +36,9 @@ bool InexorCefMenu::Set(const CefString& name, const CefRefPtr<CefV8Value> objec
     return false;
 }
 
+void InexorCefMenu::SetVisibility(bool visible) {
+    this->is_visible = visible;
+    layer->SetVisibility(visible);
+    layer->SetIsAcceptingInput(visible);
+    layer->SetFocus(visible);
+};
