36ff100c89deaa1d6b0e5149ff3d6a2f227a46ea
aschaeffer <hanack@nooblounge.net>
2015-01-13 23:39:06 +0100

    Layout independent keyboard handling

--- src/ui/cefkeyboard.cpp
+++ src/ui/cefkeyboard.cpp
@@ -28,300 +28,257 @@ bool InexorCefKeyboardManager::Set(const CefString& name, const CefRefPtr<CefV8V
 
 void InexorCefKeyboardManager::SendKeyEvent(SDL_Event event)
 {
-    bool is_char = false;
-    bool shift = false;
-    bool num_lock = false;
+    /** Modifiers **/
+    bool shift = (event.key.keysym.mod & KMOD_LSHIFT) || (event.key.keysym.mod & KMOD_RSHIFT);
+    bool ctrl = (event.key.keysym.mod & KMOD_LCTRL) || (event.key.keysym.mod & KMOD_RCTRL);
+    bool alt = (event.key.keysym.mod & KMOD_LALT) || (event.key.keysym.mod & KMOD_RALT);
+    bool num_lock = !(event.key.keysym.mod & KMOD_NUM);
+    bool caps_lock = event.key.keysym.mod & KMOD_CAPS;
+    int modifiers = 0;
+    if (shift)
+        modifiers += EVENTFLAG_SHIFT_DOWN;
+    if (ctrl)
+        modifiers += EVENTFLAG_CONTROL_DOWN;
+    if (alt)
+        modifiers += EVENTFLAG_ALT_DOWN;
+    if (num_lock)
+        modifiers += EVENTFLAG_NUM_LOCK_ON;
+    if (caps_lock)
+        modifiers += EVENTFLAG_CAPS_LOCK_ON;
+
+    /** Output codes **/
     int key_code = 0;
     int char_code = 0;
-    int which = 0;
-    int modifiers = 0;
+    bool is_char = false;
 
-    // logoutf("type: %d mod: %d sym: %d (%s) scan: %d (%s)", event.key.state, event.key.keysym.mod, event.key.keysym.sym, SDL_GetKeyName(event.key.keysym.sym), event.key.keysym.scancode, SDL_GetScancodeName(event.key.keysym.scancode));
+    if (event.type == SDL_TEXTINPUT) {
+        // logoutf("text: %s char[0]: %d", event.text.text, (int) event.text.text[0]);
 
-    /** Modifiers **/
-
-    if ((event.key.keysym.mod & KMOD_LSHIFT) || (event.key.keysym.mod & KMOD_RSHIFT)) {
-        modifiers += EVENTFLAG_SHIFT_DOWN;
-        shift = true;
-    }
-    if ((event.key.keysym.mod & KMOD_LCTRL) || (event.key.keysym.mod & KMOD_RCTRL)) {
-        modifiers += EVENTFLAG_CONTROL_DOWN;
-    }
-    if ((event.key.keysym.mod & KMOD_LALT) || (event.key.keysym.mod & KMOD_RALT)) {
-        modifiers += EVENTFLAG_ALT_DOWN;
-    }
-    if (event.key.keysym.mod & KMOD_NUM) {
-        // Num Lock is OFF!!!
-    } else {
-        num_lock = true;
-        modifiers += EVENTFLAG_NUM_LOCK_ON;
-    }
-    if (event.key.keysym.mod & KMOD_CAPS) {
-        modifiers += EVENTFLAG_CAPS_LOCK_ON;
-    }
-
-    /** Alphanumeric and keypad **/
-
-    if (event.key.keysym.sym >= SDLK_a && event.key.keysym.sym <= SDLK_z) {
         is_char = true;
-        key_code = event.key.keysym.sym;
-        if (shift) {
-            char_code = event.key.keysym.sym - 32;
-        } else {
-            char_code = event.key.keysym.sym;
-        }
-    } else if (event.key.keysym.sym >= SDLK_0 && event.key.keysym.sym <= SDLK_9) {
-        is_char = true;
-        key_code = event.key.keysym.sym;
-        if (!shift) {
-            char_code = event.key.keysym.sym;
-        } else {
-            // keyboard layout dependent
-            switch(event.key.keysym.sym)
+        if (event.text.text[0] >= 32 && event.text.text[0] <= SDLK_z)
+            char_code = event.text.text[0];
+        else
+            switch(event.text.text[0])
             {
-                case SDLK_0:
-                    char_code = JSC_EQUAL;
+                case '(':
+                    char_code = JSC_PARENTHESES_LEFT;
                     break;
-                case SDLK_1:
-                    char_code = JSC_EXCLAIM;
+                case ')':
+                    char_code = JSC_PARENTHESES_RIGHT;
                     break;
-                case SDLK_2:
-                    char_code = JSC_DOUBLE_QUOTE;
+                case '[':
+                    char_code = JSC_BRACKET_LEFT;
                     break;
-                case SDLK_3:
-                    char_code = 0;
+                case ']':
+                    char_code = JSC_BRACKET_RIGHT;
                     break;
-                case SDLK_4:
-                    char_code = JSC_DOLLAR;
-                    break;
-                case SDLK_5:
-                    char_code = JSC_PERCENT;
-                    break;
-                case SDLK_6:
-                    char_code = JSC_AMPERSAND;
-                    break;
-                case SDLK_7:
-                    char_code = JSC_SLASH;
-                    break;
-                case SDLK_8:
+                case '{':
                     char_code = JSC_BRACE_LEFT;
                     break;
-                case SDLK_9:
+                case '}':
                     char_code = JSC_BRACE_RIGHT;
                     break;
-            }
-            // char_code = SDLK_EXCLAIM;
-        }
-    } else if (event.key.keysym.sym >= _SDLK_KP0 && event.key.keysym.sym <= _SDLK_KP9) {
-        if (!num_lock) {
-            is_char = true;
-            int diff = event.key.keysym.sym - _SDLK_KP0;
-            key_code = JSK_NUM_0 + diff;
-            char_code = JSK_0 + diff;
-        } else {
-            switch (event.key.keysym.sym)
-            {
-                case _SDLK_KP0:
-                    key_code = JSK_INSERT;
+                case ',':
+                    char_code = JSC_COMMA;
                     break;
-                case _SDLK_KP1:
-                    key_code = JSK_END;
+                case ';':
+                    char_code = JSC_SEMI_COLON;
                     break;
-                case _SDLK_KP2:
-                    key_code = JSK_DOWN_ARROW;
+                case '-':
+                    char_code = JSC_SUBTRACT;
                     break;
-                case _SDLK_KP3:
-                    key_code = JSK_PAGE_DOWN;
+                case '_':
+                    char_code = JSC_UNDERSCORE;
                     break;
-                case _SDLK_KP4:
-                    key_code = JSK_LEFT_ARROW;
+                case '#':
+                    char_code = JSC_HASH;
                     break;
-                case _SDLK_KP5:
-                    key_code = JSK_NUM5_SECOND;
+                case '\'':
+                    char_code = JSC_SINGLE_QUOTE;
                     break;
-                case _SDLK_KP6:
-                    key_code = JSK_RIGHT_ARROW;
+                case '+':
+                    char_code = JSC_PLUS;
                     break;
-                case _SDLK_KP7:
-                    key_code = JSK_HOME;
+                case '*':
+                    char_code = JSC_ASTERISK;
                     break;
-                case _SDLK_KP8:
-                    key_code = JSK_UP_ARROW;
+                case '~':
+                    char_code = JSC_TILDE;
                     break;
-                case _SDLK_KP9:
-                    key_code = JSK_PAGE_UP;
+                case '?':
+                    char_code = JSC_QUESTION;
+                    break;
+                case '\\':
+                    char_code = JSC_BACK_SLASH;
+                    break;
+                case '|':
+                    char_code = JSC_PIPE;
+                    break;
+                default:
+                    // char_code = event.text.text[0];
                     break;
             }
-        }
-    } else if (event.key.keysym.sym >= _SDLK_F1 && event.key.keysym.sym <= _SDLK_F12) {
-        key_code = JSK_F1 + (event.key.keysym.sym - _SDLK_F1);
-    }
+    } else if (event.type == SDL_KEYDOWN || event.type == SDL_KEYUP) {
+        // logoutf("type: %d mod: %d sym: %d (%s) scan: %d (%s)", event.key.state, event.key.keysym.mod, event.key.keysym.sym, SDL_GetKeyName(event.key.keysym.sym), event.key.keysym.scancode, SDL_GetScancodeName(event.key.keysym.scancode));
 
-    switch(event.key.keysym.sym)
-    {
-        case SDLK_BACKSPACE:
-            key_code = JSK_BACKSPACE;
-            break;
-        case SDLK_TAB:
-            key_code = JSK_TAB;
-            break;
-        case SDLK_RETURN:
-        case _SDLK_NUM_ENTER:
-            is_char = true;
-            key_code = JSK_ENTER;
-            char_code = JSK_ENTER;
-            break;
-        case SDLK_PAUSE:
-            key_code = JSK_PAUSE;
-            break;
-        case _SDLK_SCROLL_LOCK:
-            key_code = JSK_SCROLL_LOCK;
-            break;
-        case SDLK_ESCAPE:
-            key_code = JSK_ESCAPE;
-            break;
-        case SDLK_LEFT:
-            key_code = JSK_LEFT_ARROW;
-            break;
-        case SDLK_UP:
-            key_code = JSK_UP_ARROW;
-            break;
-        case SDLK_RIGHT:
-            key_code = JSK_RIGHT_ARROW;
-            break;
-        case SDLK_DOWN:
-            key_code = JSK_DOWN_ARROW;
-            break;
-        case _SDLK_HOME:
-            key_code = JSK_HOME;
-            break;
-        case _SDLK_END:
-            key_code = JSK_END;
-            break;
-        case _SDLK_PAGEUP:
-            key_code = JSK_PAGE_UP;
-            break;
-        case _SDLK_PAGEDOWN:
-            key_code = JSK_PAGE_DOWN;
-            break;
-        case _SDLK_INSERT:
-            key_code = JSK_INSERT;
-            break;
-        case SDLK_DELETE:
-            is_char = true;
-            key_code = JSK_DELETE;
-            char_code = JSC_DELETE;
-            break;
-        case _SDLK_NUM_DIVIDE:
-            is_char = true;
-            key_code = JSK_DIVIDE;
-            char_code = JSC_NUM_DIVIDE;
-            break;
-        case _SDLK_NUM_MULTIPLY:
-            is_char = true;
-            key_code = JSK_MULTIPLY;
-            char_code = JSC_NUM_MULTIPLY;
-            break;
-        case _SDLK_NUM_SUBTRACT:
-            is_char = true;
-            key_code = JSK_SUBTRACT;
-            char_code = JSC_NUM_SUBTRACT;
-            break;
-        case _SDLK_NUM_ADD:
-            is_char = true;
-            key_code = JSK_ADD;
-            char_code = JSC_NUM_ADD;
-            break;
-        case _SDLK_NUM_DECIMAL:
-            if (num_lock) {
+        /** Function Keys **/
+        if (event.key.keysym.sym >= _SDLK_F1 && event.key.keysym.sym <= _SDLK_F12) {
+            key_code = JSK_F1 + (event.key.keysym.sym - _SDLK_F1);
+        }
+        /** Special Keys **/
+        switch(event.key.keysym.sym)
+        {
+            case SDLK_BACKSPACE:
+                key_code = JSK_BACKSPACE;
+                break;
+            case SDLK_TAB:
+                key_code = JSK_TAB;
+                break;
+            case SDLK_RETURN:
                 is_char = true;
-                key_code = JSK_DECIMAL_POINT; // keyboard layout dependent!
-                char_code = JSK_DELETE;
-            } else {
+                key_code = JSK_ENTER;
+                char_code = JSK_ENTER;
+                break;
+            case SDLK_PAUSE:
+                key_code = JSK_PAUSE;
+                break;
+            case _SDLK_SCROLL_LOCK:
+                key_code = JSK_SCROLL_LOCK;
+                break;
+            case SDLK_ESCAPE:
+                key_code = JSK_ESCAPE;
+                break;
+            case SDLK_LEFT:
+                key_code = JSK_LEFT_ARROW;
+                break;
+            case SDLK_UP:
+                key_code = JSK_UP_ARROW;
+                break;
+            case SDLK_RIGHT:
+                key_code = JSK_RIGHT_ARROW;
+                break;
+            case SDLK_DOWN:
+                key_code = JSK_DOWN_ARROW;
+                break;
+            case _SDLK_HOME:
+                key_code = JSK_HOME;
+                break;
+            case _SDLK_END:
+                key_code = JSK_END;
+                break;
+            case _SDLK_PAGEUP:
+                key_code = JSK_PAGE_UP;
+                break;
+            case _SDLK_PAGEDOWN:
+                key_code = JSK_PAGE_DOWN;
+                break;
+            case _SDLK_INSERT:
+                key_code = JSK_INSERT;
+                break;
+            case SDLK_DELETE:
+                is_char = true;
                 key_code = JSK_DELETE;
-            }
-            break;
-        case SDLK_SPACE:
-            is_char = true;
-            key_code = JSK_SPACE;
-            char_code = SDLK_SPACE;
-            break;
-        case SDLK_MINUS:
-        case SDLK_UNDERSCORE:
-            if (!shift) {
+                char_code = JSC_DELETE;
+                break;
+            case _SDLK_NUM_DIVIDE:
                 is_char = true;
-                key_code = JSK_DASH;
-                char_code = SDLK_MINUS;
-            } else {
+                key_code = JSK_DIVIDE;
+                char_code = JSC_NUM_DIVIDE;
+                break;
+            case _SDLK_NUM_MULTIPLY:
                 is_char = true;
-                key_code = JSK_DASH;
-                char_code = SDLK_UNDERSCORE;
-            }
-            break;
-        case SDLK_PERIOD:
-        case SDLK_COLON:
-            if (!shift) {
+                key_code = JSK_MULTIPLY;
+                char_code = JSC_NUM_MULTIPLY;
+                break;
+            case _SDLK_NUM_SUBTRACT:
                 is_char = true;
-                key_code = JSK_PERIOD;
-                char_code = SDLK_PERIOD;
-            } else {
+                key_code = JSK_SUBTRACT;
+                char_code = JSC_NUM_SUBTRACT;
+                break;
+            case _SDLK_NUM_ADD:
                 is_char = true;
-                key_code = JSK_PERIOD;
-                char_code = SDLK_COLON;
-            }
-            break;
-        case SDLK_COMMA:
-        case SDLK_SEMICOLON:
-            if (!shift) {
-                is_char = true;
-                key_code = JSK_COMMA;
-                char_code = SDLK_COMMA;
-            } else {
-                is_char = true;
-                key_code = JSK_COMMA;
-                char_code = SDLK_SEMICOLON;
-            }
-            break;
-        case _SDLK_CAPS_LOCK:
-            key_code = JSK_CAPS_LOCK;
-            break;
-        case _SDLK_NUM:
-            key_code = JSK_NUM_LOCK;
-            break;
-        case _SDLK_LCTRL:
-        case _SDLK_RCTRL:
-            key_code = JSK_CTRL;
-            break;
-        case _SDLK_LSHIFT:
-        case _SDLK_RSHIFT:
-            key_code = JSK_SHIFT;
-            break;
-        case _SDLK_LALT:
-            key_code = JSK_LEFT_ALT;
-            break;
-        case _SDLK_LMETA:
-            key_code = JSK_LEFT_WIN;
-            break;
-        case _SDLK_RALT:
-            key_code = JSK_RIGHT_ALT;
-            break;
-        case _SDLK_RMETA:
-            key_code = JSK_RIGHT_WIN;
-            break;
-        case _SDLK_SELECT:
-            key_code = JSK_SELECT;
-            break;
+                key_code = JSK_ADD;
+                char_code = JSC_NUM_ADD;
+                break;
+            case _SDLK_NUM_DECIMAL:
+                if (num_lock) {
+                    is_char = true;
+                    key_code = JSK_DECIMAL_POINT; // keyboard layout dependent!
+                    char_code = JSK_DELETE;
+                } else {
+                    key_code = JSK_DELETE;
+                }
+                break;
+            case _SDLK_KP0:
+                key_code = JSK_INSERT;
+                break;
+            case _SDLK_KP1:
+                key_code = JSK_END;
+                break;
+            case _SDLK_KP2:
+                key_code = JSK_DOWN_ARROW;
+                break;
+            case _SDLK_KP3:
+                key_code = JSK_PAGE_DOWN;
+                break;
+            case _SDLK_KP4:
+                key_code = JSK_LEFT_ARROW;
+                break;
+            case _SDLK_KP5:
+                key_code = JSK_NUM5_SECOND;
+                break;
+            case _SDLK_KP6:
+                key_code = JSK_RIGHT_ARROW;
+                break;
+            case _SDLK_KP7:
+                key_code = JSK_HOME;
+                break;
+            case _SDLK_KP8:
+                key_code = JSK_UP_ARROW;
+                break;
+            case _SDLK_KP9:
+                key_code = JSK_PAGE_UP;
+                break;
+            case _SDLK_CAPS_LOCK:
+                key_code = JSK_CAPS_LOCK;
+                break;
+            case _SDLK_NUM:
+                key_code = JSK_NUM_LOCK;
+                break;
+            case _SDLK_LCTRL:
+            case _SDLK_RCTRL:
+                key_code = JSK_CTRL;
+                break;
+            case _SDLK_LSHIFT:
+            case _SDLK_RSHIFT:
+                key_code = JSK_SHIFT;
+                break;
+            case _SDLK_LALT:
+                key_code = JSK_LEFT_ALT;
+                break;
+            case _SDLK_LMETA:
+                key_code = JSK_LEFT_WIN;
+                break;
+            case _SDLK_RALT:
+                key_code = JSK_RIGHT_ALT;
+                break;
+            case _SDLK_RMETA:
+                key_code = JSK_RIGHT_WIN;
+                break;
+            case _SDLK_SELECT:
+                key_code = JSK_SELECT;
+                break;
+        }
+
     }
 
     /** Still not mapped? **/
-
     if (key_code == 0) {
         key_code = event.key.keysym.sym;
     }
 
     /** Fire key events to CEF **/
-
-    if (event.key.state == SDL_PRESSED) {
+    if (event.key.state == SDL_PRESSED || event.type == SDL_TEXTINPUT) {
         // onkeydown
         CefKeyEvent key_event_key_down;
         key_event_key_down.type = KEYEVENT_KEYDOWN;
