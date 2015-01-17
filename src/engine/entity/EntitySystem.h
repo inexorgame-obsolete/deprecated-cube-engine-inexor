1e706c7b46ce70188997f4da8e04634d0b37c7ac
aschaeffer <hanack@nooblounge.net>
2015-01-17 18:32:56 +0100

    Solid entity system structure

---
+++ src/engine/entity/EntitySystem.h
@@ -0,0 +1,14 @@
+/*
+ * EntitySystem.h
+ *
+ *  Created on: 17.01.2015
+ *      Author: aschaeffer
+ */
+
+#ifndef SRC_ENGINE_ENTITY_ENTITYSYSTEM_H_
+#define SRC_ENGINE_ENTITY_ENTITYSYSTEM_H_
+
+#include "include/cef_base.h"
+#include "include/internal/cef_ptr.h"
+
+#endif /* SRC_ENGINE_ENTITY_ENTITYSYSTEM_H_ */
