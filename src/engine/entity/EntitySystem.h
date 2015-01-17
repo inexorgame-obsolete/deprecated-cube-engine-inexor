9c1e0eed5d10c11caa31157ac3b70f88e9c33796
aschaeffer <hanack@nooblounge.net>
2015-01-17 21:13:21 +0100

    Create and get entity and relationship types

--- src/engine/entity/EntitySystem.h
+++ src/engine/entity/EntitySystem.h
@@ -8,7 +8,58 @@
 #ifndef SRC_ENGINE_ENTITY_ENTITYSYSTEM_H_
 #define SRC_ENGINE_ENTITY_ENTITYSYSTEM_H_
 
-#include "include/cef_base.h"
-#include "include/internal/cef_ptr.h"
+#include "EntitySystemBase.h"
+
+class EntitySystem
+{
+    public:
+        EntitySystem();
+        virtual ~EntitySystem();
+
+        /**
+         * Creates an entity type.
+         */
+        CefRefPtr<EntityType> CreateEntityType(std::string name, bool persist, bool synchronize);
+
+        /**
+         * Creates an relationship type.
+         */
+        CefRefPtr<RelationshipType> CreateRelationshipType(std::string name, bool persist, bool synchronize);
+
+        /**
+         * Returns the entity type with the given name.
+         */
+        CefRefPtr<EntityType> GetEntityType(std::string name);
+
+        /**
+         * Returns the relationship type with the given name.
+         */
+        CefRefPtr<RelationshipType> GetRelationshipType(std::string name);
+
+    private:
+
+        /**
+         * The entity types by name.
+         */
+        std::map<std::string, CefRefPtr<EntityType> > entity_types;
+
+        /**
+         * The relationship types by name.
+         */
+        std::map<std::string, CefRefPtr<RelationshipType> > relationship_types;
+
+        /**
+         * The entity instances.
+         */
+        std::list<CefRefPtr<EntityInstance> > entity_instances;
+
+        /**
+         * The relationship instances.
+         */
+        std::list<CefRefPtr<RelationshipInstance> > relationship_instances;
+
+        // Include the default reference counting implementation.
+        IMPLEMENT_REFCOUNTING(EntitySystem);
+};
 
 #endif /* SRC_ENGINE_ENTITY_ENTITYSYSTEM_H_ */
