f279885c0c55cddd4b607be6f6dc080c37f5c106
aschaeffer <hanack@nooblounge.net>
2015-01-18 17:22:34 +0100

    Added braces operators for function calls; Added Entity and Relationship instances; Refactored type and instance attributes into AttributeBase

---
+++ src/engine/entity/AttributeBase.cpp
@@ -0,0 +1,20 @@
+/*
+ * AttributeBase.cpp
+ *
+ *  Created on: 18.01.2015
+ *      Author: aschaeffer
+ */
+
+#include "AttributeBase.h"
+
+AttributeBase::AttributeBase()
+{
+    // TODO Auto-generated constructor stub
+
+}
+
+AttributeBase::~AttributeBase()
+{
+    // TODO Auto-generated destructor stub
+}
+
--- src/engine/entity/EntityFunction.cpp
+++ src/engine/entity/EntityFunction.cpp
@@ -6,4 +6,97 @@
  */
 
 #include "EntityFunction.h"
+#include "TypeBase.h"
+#include "EntityType.h"
+#include "RelationshipType.h"
+#include "EntityInstance.h"
+#include "RelationshipInstance.h"
 
+void EntityFunction::Execute() {
+    logoutf("execute");
+};
+
+void EntityFunction::Execute(TypeBase* type)
+{
+    logoutf("execute, Type: TypeBase, Type Name: %s", type->GetName().c_str());
+};
+
+void EntityFunction::Execute(EntityType* type)
+{
+    logoutf("execute, Type: EntityType, Type Name: %s", type->GetName().c_str());
+};
+
+void EntityFunction::Execute(RelationshipType* type)
+{
+    logoutf("execute, Type: RelationshipType, Type Name: %s", type->GetName().c_str());
+};
+
+void EntityFunction::Execute(EntityInstance* inst)
+{
+    logoutf("execute, Type: EntityInstance, Type Name: %s", inst->GetType()->GetName().c_str());
+};
+
+void EntityFunction::Execute(RelationshipInstance* inst)
+{
+    logoutf("execute, Type: RelationshipInstance, Type Name: %s", inst->GetType()->GetName().c_str());
+};
+
+void EntityFunction::operator()()
+{
+    Execute();
+}
+
+void EntityFunction::operator()(TypeBase* type)
+{
+    Execute(type);
+}
+
+void EntityFunction::operator()(EntityType* type)
+{
+    Execute(type);
+}
+
+void EntityFunction::operator()(RelationshipType* type)
+{
+    Execute(type);
+}
+
+void EntityFunction::operator()(EntityInstance* inst)
+{
+    Execute(inst);
+}
+
+void EntityFunction::operator()(RelationshipInstance* inst)
+{
+    Execute(inst);
+}
+
+void FunctionRefPtr::operator()()
+{
+    get()->Execute();
+}
+
+void FunctionRefPtr::operator()(TypeBase* type)
+{
+    get()->Execute(type);
+}
+
+void FunctionRefPtr::operator()(EntityType* type)
+{
+    get()->Execute(type);
+}
+
+void FunctionRefPtr::operator()(RelationshipType* type)
+{
+    get()->Execute(type);
+}
+
+void FunctionRefPtr::operator()(EntityInstance* inst)
+{
+    get()->Execute(inst);
+}
+
+void FunctionRefPtr::operator()(RelationshipInstance* inst)
+{
+    get()->Execute(inst);
+}
