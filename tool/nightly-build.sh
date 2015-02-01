6d7eba2493f4ebf0e6fe4accaf9cd717f14fe8dd
Karolin Varner <karo@cupdev.net>
2015-02-01 16:50:48 +0100

    Refactor nightly build
    * Give the deirectory we're zipping a name
    * More detailed output

--- tool/nightly-build.sh
+++ tool/nightly-build.sh
@@ -1,34 +1,49 @@
 #!/bin/bash
 set -ev
 
-echo "branch: ${TRAVIS_BRANCH}, Job No: ${TRAVIS_JOB_NUMBER}" #e.g. "branch: master, Job No: 251.4"
-
 cd $TRAVIS_BUILD_DIR
 
-export BUILD_NAME="$(echo "${TRAVIS_BRANCH}-${TRAVIS_JOB_NUMBER}" | sed 's#/#-#g')-${TARGET}"
+# Travis does a rather bad job detecting the branch :/
+branch="$TRAVIS_BRANCH"
+jobno="$TRAVIS_JOB_NUMBER"
+build="$(echo "${branch}-${jobno}" | sed 's#/#-#g')-${TARGET}"
 
-if [ "${TRAVIS_BRANCH}" == "master" ]
-then # the master-nightly-build has the data-folder included
-    git clone --depth 1 https://github.com/inexor-game/data.git nightly
-    rm -rf ./nightly/.git*
-else
-    mkdir nightly
-fi
+outd="${build}.d/"
+zipf="${build}.zip"
 
-cp -r ./bin_* nightly
-cp ./inexor_unix nightly
-cp ./inexor.bat nightly
-cp ./server.bat nightly
-cp ./readme.md nightly
-cp ./license.md nightly
-zip -r ${BUILD_NAME}.zip nightly
+"${branch}"  == "master" && media="true" || media="false"
 
-echo "Commit: ${TRAVIS_COMMIT}" >> ${BUILD_NAME}.txt
-echo "SHA 512: " >> ${BUILD_NAME}.txt
-sha512sum ${BUILD_NAME}.zip >> ${BUILD_NAME}.txt
+echo "
+---------------------------------------
+  Exporting Nightly build
+  
+  commit: ${TRAVIS_COMMIT}
+  branch: ${branch}
+  job:    ${jobno}
+  build:  ${build}
 
-echo "sending ${BUILD_NAME}.zip"
-curl --connect-timeout 1800 --ftp-create-dirs -T "${BUILD_NAME}.zip" -u $FTP_USER:$FTP_PASSWORD ftp://inexor.org/
-curl --ftp-create-dirs -T "${BUILD_NAME}.txt" -u $FTP_USER:$FTP_PASSWORD ftp://inexor.org/
+  zip: ${zipf}
+  dir: ${outd}
+
+  data export: $media
+"
+
+mkdir "$outd"
+
+test "$media" = true && {
+  git clone --depth 1 https://github.com/inexor-game/data.git "$outd"
+  rm -rf "./${outd}/.git"*
+} || true
+
+cp -rv ./bin_* ./inexor_unix ./inexor.bat ./server.bat ./readme.md ./license.md ${outd}
+zip -r "$zipf" "$outd"
+
+echo "Commit: ${TRAVIS_COMMIT}" >> ${build}.txt
+echo "SHA 512: " >> ${build}.txt
+sha512sum "$zipf" >> ${build}.txt
+
+echo "sending ${zipf}"
+curl --connect-timeout 1800 --ftp-create-dirs -T "$zipf" -u $FTP_USER:$FTP_PASSWORD ftp://inexor.org/
+curl --ftp-create-dirs -T "${build}.txt" -u $FTP_USER:$FTP_PASSWORD ftp://inexor.org/
 
 exit 0
