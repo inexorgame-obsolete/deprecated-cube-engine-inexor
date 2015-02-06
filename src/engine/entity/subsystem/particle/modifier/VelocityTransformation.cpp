e75fea312df865407c739a06fe83172ce1ac94ef
aschaeffer <hanack@nooblounge.net>
2015-02-06 17:32:44 +0100

    Performance improvement

--- src/engine/entity/subsystem/particle/modifier/VelocityTransformation.cpp
+++ src/engine/entity/subsystem/particle/modifier/VelocityTransformation.cpp
@@ -21,9 +21,9 @@ void VelocityTransformation::Execute(TimeStep time_step, EntityInstance* modifie
 
     // Store last position
     // EntityInstance i = (*particle);
-    (*particle)["lx"] = (*particle)["x"];
-    (*particle)["ly"] = (*particle)["y"];
-    (*particle)["lz"] = (*particle)["z"];
+    (*particle)["lx"]->doubleVal = (*particle)["x"]->doubleVal;
+    (*particle)["ly"]->doubleVal = (*particle)["y"]->doubleVal;
+    (*particle)["lz"]->doubleVal = (*particle)["z"]->doubleVal;
     // Add velocity vector
     (*particle)["x"]->doubleVal = (*particle)["x"]->doubleVal + (*particle)["vx"]->doubleVal * time_step.time_factor;
     (*particle)["y"]->doubleVal = (*particle)["y"]->doubleVal + (*particle)["vy"]->doubleVal * time_step.time_factor;
