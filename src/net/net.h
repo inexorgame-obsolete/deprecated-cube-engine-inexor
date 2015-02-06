1b76e86664cc33cb93d5f36763c395a8cd20f793
Karolin Varner <karo@cupdev.net>
2015-02-07 00:53:05 +0100

    Remove deprecated include to net/StreamChopper.h
    (Why was this  not broken before oO)

--- src/net/net.h
+++ src/net/net.h
@@ -3,9 +3,15 @@
 
 #include "net/MessageConnect.h"
 #include "net/MCHub.h"
-#include "net/StreamChopper.h"
+#include "net/MCByteBuffer.h"
 #include "net/MCServer.h"
 #include "net/MCSocketServer.h"
 #include "net/MCSocket.h"
+net/MCHub.h
+net/MCServer.h
+net/MCSocket.h
+net/MCSocketServer.h
+net/MessageConnect.h
+net/net.h
 
 #endif
