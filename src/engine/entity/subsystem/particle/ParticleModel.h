5780e7311e3913a66a4b50fee7f5ad17f611634f
aschaeffer <hanack@nooblounge.net>
2015-02-07 23:12:53 +0100

    More string constants

--- src/engine/entity/subsystem/particle/ParticleModel.h
+++ src/engine/entity/subsystem/particle/ParticleModel.h
@@ -35,48 +35,40 @@ namespace particle {
     const std::string ENABLED("enabled");
     const std::string MILLIS_TO_PROCESS("millistoprocess");
 
+    const std::string ENTTYPE_PARENT_PARTICLE_TYPE("particle_type");
+    const std::string ENTTYPE_PARENT_EMITTER_TYPE("particle_emitter");
+    const std::string ENTTYPE_PARENT_INITIALIZER_TYPE("particle_initializer");
+    const std::string ENTTYPE_PARENT_MODIFIER_TYPE("particle_modifier");
+    const std::string ENTTYPE_PARENT_RENDERER_TYPE("particle_renderer");
+
+    const std::string REL_EMITTED_BY("emitted_by");
+    const std::string REL_MODIFIES("modifies");
+    const std::string REL_RENDERS("renders");
+    const std::string REL_APPLY_INITIALIZER("apply_initializer");
+    const std::string REL_APPLY_MODIFIER("apply_modifier");
+    const std::string REL_APPLY_RENDERER("apply_renderer");
+
+    const std::string PARTICLE_EMITTER_TYPE_FACTORY("particle_emitter_type_factory");
+    const std::string PARTICLE_EMITTER_FUNCTION_ATTRIBUTE_NAME("emit");
+    const std::string ENTTYPE_PREFIX_PARTICLE_EMITTER_TYPE("particle_emitter_type_");
+
+    const std::string PARTICLE_INITIALIZER_TYPE_FACTORY("particle_initializer_type_factory");
+    const std::string PARTICLE_INITIALIZER_FUNCTION_ATTRIBUTE_NAME("initialize");
+    const std::string ENTTYPE_PREFIX_PARTICLE_INITIALIZER_TYPE("particle_initializer_type_");
+
+    const std::string PARTICLE_MODIFIER_TYPE_FACTORY("particle_modifier_type_factory");
+    const std::string PARTICLE_MODIFIER_FUNCTION_ATTRIBUTE_NAME("modify");
+    const std::string ENTTYPE_PREFIX_PARTICLE_MODIFIER_TYPE("particle_modifier_type_");
+
+    const std::string PARTICLE_TYPE_FACTORY("particle_type_factory");
+    const std::string PARTICLE_TYPE_FUNCTION_ATTRIBUTE_NAME("");
+    const std::string PARTICLE_TYPE_RENDERER_INSTANCE_ATTRIBUTE_NAME("renderer_instance");
+    const std::string ENTTYPE_PREFIX_PARTICLE_TYPE("particle_type_");
+
     const std::string POINT_EMITTER_FUNCTION("point_emitter");
 
 }
 }
 }
-
-// #define POINT_EMITTER_FUNCTION "point_emitter"
-
-#define ENTTYPE_PARENT_PARTICLE_TYPE "particle_type"
-#define ENTTYPE_PARENT_EMITTER_TYPE "particle_emitter"
-#define ENTTYPE_PARENT_INITIALIZER_TYPE "particle_initializer"
-#define ENTTYPE_PARENT_MODIFIER_TYPE "particle_modifier"
-#define ENTTYPE_PARENT_RENDERER_TYPE "particle_renderer"
-
-#define REL_EMITTED_BY "emitted_by"
-#define REL_MODIFIES "modifies"
-#define REL_RENDERS "renders"
-#define REL_APPLY_INITIALIZER "apply_initializer"
-#define REL_APPLY_MODIFIER "apply_modifier"
-#define REL_APPLY_RENDERER "apply_renderer"
-
-#define PARTICLE_EMITTER_TYPE_FACTORY "particle_emitter_type_factory"
-#define PARTICLE_EMITTER_FUNCTION_ATTRIBUTE_NAME "emit"
-#define ENTTYPE_PREFIX_PARTICLE_EMITTER_TYPE "particle_emitter_type_"
-
-#define PARTICLE_INITIALIZER_TYPE_FACTORY "particle_initializer_type_factory"
-#define PARTICLE_INITIALIZER_FUNCTION_ATTRIBUTE_NAME "initialize"
-#define ENTTYPE_PREFIX_PARTICLE_INITIALIZER_TYPE "particle_initializer_type_"
-
-#define PARTICLE_MODIFIER_TYPE_FACTORY "particle_modifier_type_factory"
-#define PARTICLE_MODIFIER_FUNCTION_ATTRIBUTE_NAME "modify"
-#define ENTTYPE_PREFIX_PARTICLE_MODIFIER_TYPE "particle_modifier_type_"
-
-#define PARTICLE_TYPE_FACTORY "particle_type_factory"
-#define PARTICLE_TYPE_FUNCTION_ATTRIBUTE_NAME ""
-#define PARTICLE_TYPE_RENDERER_INSTANCE_ATTRIBUTE_NAME "renderer_instance"
-#define ENTTYPE_PREFIX_PARTICLE_TYPE "particle_type_"
-
-// #define ENTTYPE_PARTICLE_EMITTER "particle_emitter"
-// #define ENTTYPE_PARTICLE_INITIALIZER "particle_initializer"
-// #define ENTTYPE_PARTICLE_MODIFIER "particle_modifier"
-// #define RELTYPE_APPLY_MODIFIER_ON_EMITTED_PARTICLES "apply_modifier_on_emitted_particles"
-// #define RELTYPE_APPLY_INITIALIZER_ON_EMITTED_PARTICLES "apply_initializer_on_emitted_particles"
 
 #endif /* SRC_ENGINE_ENTITY_SUBSYSTEM_PARTICLE_PARTICLEMODEL_H_ */
