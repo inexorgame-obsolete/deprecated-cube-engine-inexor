5d120010ef5d3232e175d63e2bced4d7ad85ef68
Karolin Varner <karo@cupdev.net>
2015-02-09 22:36:35 +0100

    Rm MCHub::MCHub(); add one that imports any sequence
    The one that imports any sequence provides a template
    abstraction; it should be able to handle vector<MCSocket>
    for instance.

--- src/net/MCHub.h
+++ src/net/MCHub.h
@@ -13,25 +13,40 @@ namespace net {
    *
    * Supports listening for Messages on the attached
    * connects and broadcasting messages to the attached
-   * connects. 
+   * connects.
    */
   class MCHub : public MessageConnect {
     /** All the connects that are attached to this */
     std::list<MessageConnect*> *cons;
-  public:
-    /**
-     * Create a new MCHub with no attached connects.
-     * connects() will return an empty list.
-     */
-    MCHub() {
-      cons = new std::list<MessageConnect*>();
-    }
 
+    /// We want ownership over the list for the
+    /// template/non-pointer constructor.
+    std::list<MessageConnect*> mine_cons;
+  public:
     /**
      * Create a new MCHub and set the list of attached
      * connects.
      */
     MCHub(std::list<MessageConnect*> *cons_) : cons(cons_) {}
+
+    /// Create a MCHub from any sequence type that
+    /// contains any type of MessageConnects (not the pointers)
+    /// In this case we will get all the pointers of the
+    /// MessageConnects and copy them into a new list;
+    ///
+    /// Future changes to the list will be ignored.
+    /// This class retains ownership over cons when using
+    /// this constructor.
+    ///
+    /// @tparam Iterable The type of container the connects
+    ///     are stored in; could be a list or a vector.
+    /// @tparam Connect The type of MessageConnect we're
+    ///     dealing with
+    /// @param list The connects to import
+    template<typename Iterable>
+    MCHub(Iterable &list) : cons(&mine_cons) {
+      for (auto &con : list) mine_cons.push_back(&con);
+    }
 
     virtual bytes_on_channel ReceiveChannel();
     virtual void Send(std::vector<char> &dat);
