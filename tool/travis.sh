ef71fe03be52de2fcc2520c0a5c45754be16a06f
Karolin Varner <karo@cupdev.net>
2015-04-03 17:22:26 +0200

    travis: Move nightly build script to travis.sh

--- tool/travis.sh
+++ tool/travis.sh
@@ -57,18 +57,102 @@ root_install() {
   )
 }
 
+upload() {
+  local dest="ftp://inexor.org/$2/$1"
+  echo >&2 "Uploading: `readlink -f "$1"` to $dest"
+  curl --connect-timeout 1800 --ftp-create-dirs \
+    -u "$FTP_USER:$FTP_PASSWORD" "$dest" -T "$1"
+}
+
+upload_file() {
+  (
+    local d="`dirname $1`" n="`basename "$1"`"
+    cd "$d"
+    upload "$n" "$2"
+  )
+}
+
+nigthly_build() {
+  local outd="/tmp/${build}.d/"
+  local zipf="/tmp/${build}.zip"
+  local descf="/tmp/${build}.txt"
+
+  # Include the media files
+  local media="${media}"
+  test -n "${media}" || test "$branch" = master && media=true
+
+  echo "
+  ---------------------------------------
+    Exporting Nightly build
+
+    commit: ${commit}
+    branch: ${branch}
+    job:    ${jobno}
+    build:  ${build}
+
+    gitroot: ${gitroot}
+    zip: ${zipf}
+    dir: ${outd}
+
+    data export: $media
+  "
+
+  mkdir "$outd"
+
+  if test "$media" = true; then (
+    cd "$outd"
+    git clone --depth 1 https://github.com/inexor-game/data.git data
+    rm -rf data/.git/
+  ) fi
+
+  cp -rv ./bin_* ./inexor_unix ./inexor_cipc_unix ./inexor.bat ./server.bat ./readme.md ./license.md "${outd}"
+
+  (
+    cd "`dirname "$outd"`"
+    zip -r "$zipf" "`basename $outd`"
+  )
+
+  (
+    echo "Commit: ${comitt}"
+    echo -n "SHA 512: "
+    sha512sum "$zipf"
+  ) > "$descf"
+
+  echo "Uploading: ${zipf} and $descf"
+  upload_file "$zipf" &
+  upload_file "$descf"
+
+  return 0
+}
+
 target_before_install() {
   sudo "$script" root_install
 }
 
 target_script() {
   (
-    mkcd cmakebuild
-    cmake $CMAKE_FLAGS ..
+    mkcd "/tmp/inexor-build-${build}"
+    cmake $CMAKE_FLAGS "$gitroot"
     make -kj 5 install
   )
   run_tests
 }
+
+# Upload nightly
+target_after_success() {
+  nigthly_build || true
+}
+
+## MAIN ####################################################
+
+export branch="$TRAVIS_BRANCH" # The branch we're on
+export jobno="$TRAVIS_JOB_NUMBER" # The job number
+export comitt="${TRAVIS_COMMIT}"
+# Name of this build
+export build="$(echo "${branch}-${jobno}" | sed 's#/#-#g')-${TARGET}"
+export gitroot="$TRAVIS_BUILD_DIR"
+
+cd "$gitroot"
 
 # Call the desired function
 "$@"
