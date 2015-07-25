emilis/inexor-code

Make all the Cubescript variables observable.
We create a new type SharedVar, which should back all
variables that can be shared among multiple environments (at
the moment C++ and Cubescript; later IPC).
This type is only an alias for inexor::util::Observe atm.

Observe is pretty clever, so this does not actually require
lot's of code changes.
Since observe overloads the cast operator, problems can
still arise where casting opportunities are ambiguities.
This is the case with min/max/clamp; that's why Observe has
overloads for them.

In Inexor – outputting them is a problem, because we
mostly use C-Style variadic functions for output. And there
we can not have complex types or type conversion. Hence we
need to dereference (*) them manually.

We also define a special overload for `void delete [](Observe<char*> &op)`,
because inexor uses char* to back cubescript strings, and
these had been manually deleted.

Everything would be a lot easier if this was proper c++
code.

inexor/engine/sound.cpp
@@ -205,7 +205,7 @@ void startmusic(char *name, char *cmd)
     stopmusic();
     if(soundvol && musicvol && *name)
     {
-        defformatstring(file)("%s/%s", musicdir, name);
+        defformatstring(file)("%s/%s", *musicdir, name);
         path(file);
         if(loadmusic(file))
         {
@@ -473,14 +473,14 @@ static bool loadsoundslot(soundslot &slot, bool msg = false)
     string filename;
     loopi(sizeof(exts)/sizeof(exts[0]))
     {
-        formatstring(filename)("%s/%s%s", sounddir, slot.sample->name, exts[i]);
+        formatstring(filename)("%s/%s%s", *sounddir, slot.sample->name, exts[i]);
         if(msg && !i) renderprogress(0, filename);
         path(filename);
         slot.sample->chunk = loadwav(filename);
         if(slot.sample->chunk) return true;
     }
 
-    conoutf(CON_WARN, "failed to load sample: %s/%s", sounddir, slot.sample->name);
+    conoutf(CON_WARN, "failed to load sample: %s/%s", *sounddir, slot.sample->name);
     return false;
 }
 

inexorgame-obsolete/deprecated-cube-engine-inexor

inexor/engine/sound.cpp
@@ -205,7 +205,7 @@ void startmusic(char *name, char *cmd)
     stopmusic();
     if(soundvol && musicvol && *name)
     {
-        defformatstring(file)("%s/%s", musicdir, name);
+        defformatstring(file)("%s/%s", *musicdir, name);
         path(file);
         if(loadmusic(file))
         {
@@ -473,14 +473,14 @@ static bool loadsoundslot(soundslot &slot, bool msg = false)
     string filename;
     loopi(sizeof(exts)/sizeof(exts[0]))
     {
-        formatstring(filename)("%s/%s%s", sounddir, slot.sample->name, exts[i]);
+        formatstring(filename)("%s/%s%s", *sounddir, slot.sample->name, exts[i]);
         if(msg && !i) renderprogress(0, filename);
         path(filename);
         slot.sample->chunk = loadwav(filename);
         if(slot.sample->chunk) return true;
     }
 
-    conoutf(CON_WARN, "failed to load sound: %s/%s", sounddir, slot.sample->name);
+    conoutf(CON_WARN, "failed to load sound: %s/%s", *sounddir, slot.sample->name);
     return false;
 }
 