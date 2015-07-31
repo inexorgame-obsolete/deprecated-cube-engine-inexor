3b025674655dd1cea64e4d82f75e22ec01031b76
eihrul <eihrul@07af1644-1164-4067-b897-63bf4dc9b748>
2015-07-31 20:38:32 +0000

    add missing ulong type
    git-svn-id: http://svn.code.sf.net/p/sauerbraten/code@5194 07af1644-1164-4067-b897-63bf4dc9b748

--- inexor/shared/tools.h
+++ inexor/shared/tools.h
@@ -13,6 +13,7 @@
 typedef unsigned char uchar;
 typedef unsigned short ushort;
 typedef unsigned int uint;
+typedef unsigned long ulong;
 typedef signed long long int llong;
 typedef unsigned long long int ullong;
 
