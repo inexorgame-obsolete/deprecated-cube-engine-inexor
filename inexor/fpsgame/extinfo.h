2c755346b9f8c2cb23898c247c6ab11c27c18c41
eihrul <eihrul@07af1644-1164-4067-b897-63bf4dc9b748>
2015-08-02 20:27:19 +0000

    make extinfo ip controlled by extinfoip var
    git-svn-id: http://svn.code.sf.net/p/sauerbraten/code@5198 07af1644-1164-4067-b897-63bf4dc9b748

--- inexor/fpsgame/extinfo.h
+++ inexor/fpsgame/extinfo.h
@@ -34,6 +34,8 @@
     B:C:default: 0 command EXT_ACK EXT_VERSION EXT_ERROR
 */
 
+    VAR(extinfoip, 0, 0, 1);
+
     void extinfoplayer(ucharbuf &p, clientinfo *ci)
     {
         ucharbuf q = p;
@@ -52,8 +54,11 @@
         putint(q, ci->state.gunselect);
         putint(q, ci->privilege);
         putint(q, ci->state.state);
-        uint ip = getclientip(ci->clientnum);
-        q.put((uchar*)&ip, 3);
+        if(extinfoip)
+        {
+            uint ip = getclientip(ci->clientnum);
+            q.put((uchar*)&ip, 3);
+        }
         sendserverinforeply(q);
     }
 
