d5263c6a38736645789978134a4a73cf77c7bc06
a_teammate <madoe3@web.de>
2015-12-07 20:28:38 +0100

    windows: use the same folder structure as on linux
    by searching through the subfolder media and adding package dirs for every found folder (done in inexor.bat).

--- inexor.bat
+++ inexor.bat
@@ -1,4 +1,5 @@
 @ECHO OFF
+Setlocal EnableDelayedExpansion
 
 set INEXOR_ARCH=win32
 
@@ -9,4 +10,9 @@ IF /I "%PROCESSOR_ARCHITEW6432%" == "amd64" (
     set INEXOR_ARCH=win64
 )
 
-start bin\windows\%INEXOR_ARCH%\inexor.exe "-q$HOME\My Games\Inexor" -glog.txt %*
+:: Clear variable
+set "DATA_DIRS="
+:: Find all data repos in media
+for /f "delims=" %%a in ('dir "./media" /on /ad /b') do @set "DATA_DIRS=!DATA_DIRS! -kmedia/%%a/"
+
+start bin\windows\%INEXOR_ARCH%\inexor.exe "-q$HOME\My Games\Inexor" %DATA_DIRS% -glog.txt %*
