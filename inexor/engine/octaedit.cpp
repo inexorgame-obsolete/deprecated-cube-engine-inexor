1a04f4b5891cc52ea9a32982509ddc738fb1aedc
Basile Henry <basile.henry@hotmail.com>
2015-12-10 17:59:05 +0000

    added commit structs

--- inexor/engine/octaedit.cpp
+++ inexor/engine/octaedit.cpp
@@ -2766,7 +2766,7 @@ bool cube_equal(cube &a, cube &b)
     return true;
 }
 
-std::vector<altcubes> cube_diff(cube &work, cube &cur, cube &ref, int size, ivec pos)
+std::vector<altcubes> cube_diff(const cube &work, const cube &cur, const cube &ref, int size, ivec pos)
 {
     std::vector<altcubes> diff, self;
     self.push_back({&work, &cur, &ref, 0, size, pos});
@@ -2895,7 +2895,7 @@ void vc_diff()
         }
     }
 
-    commitchanges();
+    commitchanges(); // submit octree changes
 }
 
 void nextconflict()
@@ -2935,7 +2935,7 @@ void nextconflict()
         colordiffs();
         loopi(diffs.size()) changeddiff(i);
         selectdiff(curconflict);
-        commitchanges();
+        commitchanges(); // submit octree changes
     }
 }
 COMMAND(nextconflict, "");
@@ -2979,3 +2979,166 @@ void diffpaste() {}
 //     pasteblock(b, sel, true);
 // }
 
+struct commit_metadata
+{
+    uint prevcommitcrc;
+    char author[16];
+    char message[100];
+    // TODO timestamp
+
+    commit_metadata(char *a, char *m, uint p) : prevcommitcrc(p)
+    {
+        std::strncpy(author, a, 15); // overflow safe
+        author[15] = '\0';
+        std::strncpy(message, m, 99); // overflow safe
+        message[99] = '\0';
+    }
+    ~commit_metadata() { DELETEA(author); DELETEA(message) }
+};
+
+typedef boost::tuple<cube*,vec3,int> cubepossize;
+
+struct commit
+{
+    commit_metadata* cm;
+    
+    // content of the commit: either a snapshot (the whole octree) or a vector of diffs
+    cube* snapshot;
+    std::vector<cubepossize> diffs;
+
+    commit(commit_metadata* cm, cube* c) : commit_metadata(cm)
+    {
+        snapshot = newcubes(F_SOLID);
+        loopi(8) copycube(worldroot[i], snapshot[i]);
+    }
+    commit(commit_metadata* cm, std::vector<cubepossize> d) : commit_metadata(cm), diffs(d) {}
+
+    ~commit()
+    {
+        diffs.clear();
+        if (snapshot)
+        {
+            loopi (8) discardchildren(snapshot[i]);
+            DELETEP(snapshot)
+        }
+    }
+};
+
+struct commit_header
+{
+    char magic[4] = {'C', 'M', 'T', 'H'};  // CMTH for .cmt header
+    uint prevcommitcrc;
+    char author[16];
+    char message[100];
+
+    commit_header(commit_metadata* cm) : prevcommitcrc(cm.prevcommitcrc), author(cm.author), message(cm.message) {}
+};
+
+static hashtable<vccommit, uint> vccommits;
+
+void savesnapshot(char *author, char *message, cube* snapshot)
+{
+    if(!author[0] || !message[0] || !snapshot || noedit(true) || (nompedit && multiplayer())) return;
+
+    commit_metadata cmmd(author, message, 0 /* TODO */);
+    commit_header hdr(cmmd);
+    commit cmt(cmmd, snapshot);
+
+
+}
+
+void savecommit(char *author, char *message, std::vector<altcubes> diffs)
+{
+    if(!author[0] || !message[0] || !diffs.size() || noedit(true) || (nompedit && multiplayer())) return;
+
+    commit_metadata cmmd(author, message, 0 /* TODO */);
+    commit_header hdr(cmmd);
+
+    std::vector<cubepossize> d;
+    loopi (diffs.size)
+    {
+        // r == 0: possibly raise an error/abort commit
+        if (diffs[i].r == 0) d.push_back(cubepossize(diffs[i].work, diffs[i].pos, diffs[i].size))
+        if (diffs[i].r == 1) d.push_back(cubepossize(diffs[i].cur, diffs[i].pos, diffs[i].size))
+        if (diffs[i].r == 2) d.push_back(cubepossize(diffs[i].ref, diffs[i].pos, diffs[i].size))
+    }
+    commit cmt(cmmd, snapshot);
+
+// static string buf;
+//     int format = -1, dirlen = 0;
+//     copystring(buf, screenshotdir);
+//         dirlen = strlen(buf);
+//         if(buf[dirlen] != '/' && buf[dirlen] != '\\' && dirlen+1 < (int)sizeof(buf)) { buf[dirlen] = '/'; buf[dirlen+1] = '\0'; }
+//         const char *dir = findfile(buf, "w");
+//         if(!fileexists(dir, "w")) createdir(dir);
+//     }
+//     if(filename[0])
+//     {
+//         concatstring(buf, filename);
+//         format = guessimageformat(buf, -1);
+//     }
+//     else
+//     {
+//         const char *sstime = gettimestr();
+//         concatstring(buf, sstime);
+
+//         const char *map = game::getclientmap()
+    // defformatstring(filename, "%s/%s.obr", *prefabdir, name);
+    // path(filename);
+
+}
+
+
+// void saveprefab(char *name)
+// {
+//     if(!name[0] || noedit(true) || (nompedit && multiplayer())) return;
+//     prefab *b = prefabs.access(name);
+//     if(!b)
+//     {
+//         b = &prefabs[name];
+//         b->name = newstring(name);
+//     }
+//     if(b->copy) freeblock(b->copy);
+//     protectsel(b->copy = blockcopy(block3(sel), sel.grid));
+//     changed(sel);
+//     defformatstring(filename, "%s/%s.obr", *prefabdir, name);
+//     path(filename);
+//     stream *f = opengzfile(filename, "wb");
+//     if(!f) { conoutf(CON_ERROR, "could not write prefab to %s", filename); return; }
+//     prefabheader hdr;
+//     memcpy(hdr.magic, "OEBR", 4);
+//     hdr.version = 0;
+//     lilswap(&hdr.version, 1);
+//     f->write(&hdr, sizeof(hdr));
+//     streambuf<uchar> s(f);
+//     if(!packblock(*b->copy, s)) { delete f; conoutf(CON_ERROR, "could not pack prefab %s", filename); return; }
+//     delete f;
+//     conoutf("wrote prefab file %s", filename);
+// }
+
+void loadcommit(uint crc)
+{
+//     if(!name[0] || noedit() || (nompedit && multiplayer())) return;
+//     prefab *b = prefabs.access(name);
+//     if(!b)
+//     {
+//         defformatstring(filename, "%s/%s.obr", *prefabdir, name);
+//         path(filename);
+//         stream *f = opengzfile(filename, "rb");
+//         if(!f) { conoutf(CON_ERROR, "could not read prefab %s", filename); return; }
+//         prefabheader hdr;
+//         if(f->read(&hdr, sizeof(hdr)) != sizeof(prefabheader) || memcmp(hdr.magic, "OEBR", 4)) { delete f; conoutf(CON_ERROR, "prefab %s has malformatted header", filename); return; }
+//         lilswap(&hdr.version, 1);
+//         if(hdr.version != 0) { delete f; conoutf(CON_ERROR, "prefab %s uses unsupported version", filename); return; }
+//         streambuf<uchar> s(f);
+//         block3 *copy = NULL;
+//         if(!unpackblock(copy, s)) { delete f; conoutf(CON_ERROR, "could not unpack prefab %s", filename); return; }
+//         delete f;
+//         b = &prefabs[name];
+//         b->name = newstring(name);
+//         b->copy = copy;
+//     }
+//     pasteblock(*b->copy, sel, true);
+}
+
+void applycommit(uint crc) {}
