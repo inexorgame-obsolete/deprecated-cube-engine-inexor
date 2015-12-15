8152a9503b075ca1228106d46280ff542b08bdef
IAmNotHanni <cmdfreak@web.de>
2015-12-15 01:41:13 +0100

    removed unnecesary initialisation in constructor. Whitespace and comment fixes.

--- inexor/flowgraph/node/fl_nodebase.cpp
+++ inexor/flowgraph/node/fl_nodebase.cpp
@@ -4,19 +4,17 @@
 namespace inexor {
 namespace vscript {
 
+
     CScriptNode::CScriptNode()
     {
         pos = vec(0,0,0);
         type = NODE_TYPE_INVALID;
-        active = true;       
         node_name = "";
-        node_comment = "";        
+        node_comment = "";
         default_box_color = VSCRIPT_COLOR_TIMER; 
         box_color = default_box_color;
         pos_changed = false;
         selected = false;
-        done_pointer = nullptr;
-        script_execution_start = 0;
     }
 
 
@@ -35,20 +33,14 @@ namespace vscript {
     {
         if(children.size() == 0)
         {
-            // this is a dead end in code!
-            /*if(type != NODE_TYPE_TIMER)
-            {
-                *done_pointer = true;
-            }*/
+            /// TODO: end thread(?)
         }
         else
         {
-            /// TODO: implement return value buffer!
             for(unsigned int i=0; i<children.size(); i++)
             {
-                children[i]->script_execution_start = script_execution_start;
                 children[i]->in();
-            }    
+            }
         }
     }
 
