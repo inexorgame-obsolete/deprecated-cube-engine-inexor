493d5b0fc79b34480215fd775ba34292c8b19458
IAmNotHanni <cmdfreak@web.de>
2015-12-15 16:23:48 +0100

    bezier curve engine refactoring. Removed unneccesary documentation/comments.

--- inexor/geom/curves/bezier/bezier.h
+++ inexor/geom/curves/bezier/bezier.h
@@ -1,14 +1,13 @@
 /// @file BezierCurve.h
-/// @brief Bezier curve generator engine
+/// @brief Bezier curve engine
 /// @author Johannes "Hanni" Schneider
 /// 
 /// IN HONOR OF
-/// 
-/// Pierre Etienne BEZIER       (September 1, 1910 - November 25, 1999), French mathematician and engineer at Renault
-/// Paul de CASTELJAU           (November 19, 1930), French mathematician and physicist  and engineer ar Citroen
-/// Sergei Natanovich BERNSTEIN (March 5, 1880 - October 26, 1968), Russian mathematician
-/// Charles HERMITE             (December 24, 1822 - January 14, 1901), French mathematician
-/// Leonardo FIBONACCI          (~1170, ~1240), Italian mathematician
+/// Pierre Etienne BEZIER       (September 1, 1910 - November 25, 1999)  French mathematician and engineer at Renault
+/// Paul de CASTELJAU           (November 19, 1930),                     French mathematician, physicist, and engineer ar Citroen
+/// Sergei Natanovich BERNSTEIN (March 5, 1880 - October 26, 1968)       Russian mathematician
+/// Charles HERMITE             (December 24, 1822 - January 14, 1901)   French mathematician
+/// Leonardo FIBONACCI          (~1170, ~1240)                           Italian mathematician
 ///
 /// A collection of useful links and documentations:
 /// http://pomax.github.io/bezierinfo/
@@ -19,9 +18,8 @@
 /// https://geom.ivd.kit.edu/downloads/pubs/pub-boehm-prautzsch_2002_preview.pdf
 /// https://www.clear.rice.edu/comp360/lectures/BezSubd.pdf
 
-
 /// TODO: Is DeCasteljau too dangerous because it is recursive?
-/// TODO: Generate a random curve?
+/// TODO: Maybe generate a random curve?
 /// TODO: Use generic types (templates)
 /// TODO: Create a curve chain manager (CBezierCurveManager)
 
@@ -37,16 +35,6 @@ namespace inexor {
 namespace geom {
 
     /// @brief algorithm type enumeration
-    /// There are two ways to generate a bezier curves from a group of [n] points. Both ways will be implemented in this engine.
-    /// 
-    /// ALGORITHM_BERNSTEIN_POLYNOM (Bernstein Polynomials)
-    /// -not recursive
-    /// -will automaticly compute and norm the tangent and normal vectors for every computed point
-    /// 
-    /// ALGORITHM_DECASTELJAU (De-Casteljau-Algorithm)
-    /// -recursive! use with caution!
-    /// -possibly needs a lot more memory for every sub-call (?)
-    /// -proven to be a little faster than the other algorithm
     enum BEZIER_ALGORITHM
     {
 	    ALGORITHM_BERNSTEIN_POLYNOM,
@@ -56,12 +44,9 @@ namespace geom {
 
     /// A bezier curve (named after french mathematician PIERRE ETIENNE BEZIER) is a parametric curve
     /// whose only purpose is to look soft and smooth. Bezier curves are all about their elegance.
-    /// Those curves can be used to represent everything you can imagine: particles, cameras, mapmodels and much more.
-    /// Bezier curves are fast, flexible, beautiful and easy to compute. You just pass a bunch of parameter points to
-    /// this engine and the final curve will be computed. Because every complex curve can be represented with a 
-    /// chain of smaller curves, it is recommended to create a chain of curves. See CBezierCurveManager.
-    /// Bezier curves are ESSENTIAL AND FUNDAMENTAL in the field of computer graphics and data/image processing.
-    /// They can also be used for approximation, interpolation and more.
+    /// Those curves can be used to represent the position of anything you want.
+    /// Bezier curves are fast, flexible, beautiful and easy to compute.
+    /// Bezier curves are essential in the field of computer graphics and data/image processing.
     class CBezierCurve : public CCurveBase
     {
         protected:
