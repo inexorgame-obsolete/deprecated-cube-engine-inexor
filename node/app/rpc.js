2a77e1f103c1b2c98eb2fd1ed778d67c975b65d7
Fohlen <lennard@berger.work>
2016-02-29 21:58:32 +0100

    Fixed response stuff with promises

--- node/app/rpc.js
+++ node/app/rpc.js
@@ -7,13 +7,15 @@ var messages = service.messages;
 // This is a specific express-middleware for proccessing RPC/Cubescript calls
 // 1. Construct a new CubeScript message buffer
 // 2. Send it via the Inexor Service Provider
-// 3. Convert the response to CubeScript message buffer and return it
+// 3. Return stuff!
 function EvalCubescript(code) {  
-    var msg = new messages.Cubescript(code);
-    return new Promise(service.EvalCubescript(msg, function(a) {
-        console.log(a);
-        return a;
-    }));
+    return new Promise(function(resolve, reject) {
+        var msg = new messages.Cubescript(code);
+        service.EvalCubescript(msg, function(data) { 
+            resolve(data); 
+        }); 
+    });
 }
+
 
 module.exports = Promise.nodeify(EvalCubescript);
