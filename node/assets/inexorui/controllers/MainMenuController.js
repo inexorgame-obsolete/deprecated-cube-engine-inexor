f5657acd37fdf90edacc46f3326aa3564ac3ab83
aschaeffer <hanack@nooblounge.net>
2016-02-29 22:38:46 +0100

    WebUI: connect and disconnect via REST API (we've closed the circle!)

--- node/assets/inexorui/controllers/MainMenuController.js
+++ node/assets/inexorui/controllers/MainMenuController.js
@@ -1,20 +1,51 @@
 define(['./module'], function(controllers) {
   'use strict';
-  controllers.controller('MainMenuController', ['$scope', '$state', 'MenuService',
-    function ($scope, $state, MenuService) {
+  controllers.controller('MainMenuController', ['$scope', '$state', '$http', 'MenuService',
+    function ($scope, $state, $http, MenuService) {
 
-      $scope.data = "The main menu";
+      $scope.message = "The main menu";
+      $scope.connected = false;
+      $scope.host = "inexor.nooblounge.net";
+      $scope.port = "31415";
+      $scope.clientIcon = "";
 
       $scope.func = function() {
       	MenuService.func();
       };
 
       $scope.openConsole = function() {
-    	  $state.go('/console');
+        $state.go('/console');
+      };
+
+      $scope.getClientIcon = function() {
+        $http.post("/execute", {
+          code: "getclienticon"
+        }).then(function(response) {
+  	      console.log(response);
+  	      $scope.clientIcon = response.data;
+  	    });  
+      };
+
+      $scope.connect = function() {
+        $http.post("/api/execute", {
+          code: "connect " + $scope.host + " " + $scope.port
+        }).then(function(response) {
+  	      console.log(response);
+          $scope.connected = true;
+        });  
+      };
+
+      $scope.disconnect = function() {
+        $http.post("/api/execute", {
+          code: "disconnect"
+        }).then(function(response) {
+          console.log(response);
+          $scope.connected = false;
+        });  
       };
 
       $scope.changeText = function() {
-    	  $scope.data = "Text changed!";
+    	  $scope.message = "Text changed!";
       };
 
       console.log("MainMenuController initialized!");
