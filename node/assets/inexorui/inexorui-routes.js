a3442bb3314defddb792535e626eb01adfea7acb
aschaeffer <hanack@nooblounge.net>
2016-02-29 20:10:41 +0100

    Added angularjs keyboard test controller

--- node/assets/inexorui/inexorui-routes.js
+++ node/assets/inexorui/inexorui-routes.js
@@ -13,6 +13,11 @@ define(['./inexorui'], function (inexorui) {
         url: '/console',
         templateUrl: 'inexorui/partials/console.html',
         controller: 'ConsoleController'
+      })
+      .state('/test/keyboard', {
+        url: '/test/keyboard',
+        templateUrl: 'inexorui/partials/test/keyboard.html',
+        controller: 'KeyboardTestController'
       });
   });
 });
