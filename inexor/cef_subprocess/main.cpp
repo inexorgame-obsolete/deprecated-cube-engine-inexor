792779270c7563747b029c80282ba788db9ecfd3
aschaeffer <hanack@nooblounge.net>
2016-03-02 00:12:42 +0100

    Consistent naming

--- inexor/cef_subprocess/main.cpp
+++ inexor/cef_subprocess/main.cpp
@@ -1,7 +1,7 @@
 #include "inexor/cef_subprocess/main.hpp"
 
 int main(int argc, char **argv) {
-    CefMainArgs ma{argc, argv};
-    CefRefPtr<InexorCefApp> inexor_cef_app = new InexorCefApp{1980, 1080};
-    return CefExecuteProcess(ma, inexor_cef_app.get(), NULL);
+    const CefMainArgs main_args(argc, argv);
+    CefRefPtr<InexorCefApp> cef_app = new InexorCefApp{1980, 1080};
+    return CefExecuteProcess(main_args, cef_app.get(), NULL);
 }
