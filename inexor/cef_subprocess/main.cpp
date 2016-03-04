6425a9e3d5fd1acb9af85d90791c6a47d562fc8d
aschaeffer <hanack@nooblounge.net>
2016-03-04 22:25:39 +0100

    windows compatibility fix for the chromium embedded framework subprocess

--- inexor/cef_subprocess/main.cpp
+++ inexor/cef_subprocess/main.cpp
@@ -1,7 +1,11 @@
 #include "inexor/cef_subprocess/main.hpp"
 
 int main(int argc, char **argv) {
+#ifdef WIN32
+    CefMainArgs main_args(GetModuleHandle(NULL));
+#else
     const CefMainArgs main_args(argc, argv);
+#endif
     CefRefPtr<InexorCefApp> cef_app(new InexorCefApp(1980, 1080));
     return CefExecuteProcess(main_args, cef_app.get(), NULL);
 }
