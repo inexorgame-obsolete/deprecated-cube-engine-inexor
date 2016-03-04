a1cb98d1117cfeff0776969b0a95ddaa6af6ab3a
Fohlen <lennard@berger.work>
2016-03-04 15:54:19 +0100

    Fixed dependencies, add bower stuff

---
+++ node/assets/config.js
@@ -0,0 +1,13 @@
+require.config({
+  shim: {
+
+  },
+  paths: {
+    bootstrap: "bower_components/bootstrap/dist/js/bootstrap",
+    "jq-console": "bower_components/jq-console/karma.conf",
+    jquery: "bower_components/jquery/dist/jquery"
+  },
+  packages: [
+
+  ]
+});
