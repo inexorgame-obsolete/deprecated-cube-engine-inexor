91477eeab6d055164e68e9c6fc195eb3258c99ab
aschaeffer <hanack@nooblounge.net>
2016-03-05 18:48:47 +0100

    CEF branch 2357 compatibility (changes in platform repository required; breaks with CEF branch 2171)

--- inexor/cef_subprocess/main.hpp
+++ inexor/cef_subprocess/main.hpp
@@ -1,6 +1,8 @@
 #ifndef INEXOR_CEF_SUBPROCESS_MAIN_HEADER
 #define INEXOR_CEF_SUBPROCESS_MAIN_HEADER
 
+#include <iostream>
+
 #include <include/cef_base.h>
 
 #include "inexor/ui/InexorCefApp.hpp"
--- inexor/ui/InexorSettings.hpp
+++ inexor/ui/InexorSettings.hpp
@@ -16,7 +16,7 @@
 class InexorSettings : public CefSettings {
     public:
 	    InexorSettings(std::string executable_path) {
-	        no_sandbox = true;
+	        no_sandbox = false;
 	    	ignore_certificate_errors = false;
 	        multi_threaded_message_loop = false;
 	        windowless_rendering_enabled = true;
@@ -30,7 +30,7 @@ class InexorSettings : public CefSettings {
 	        CefString(&locales_dir_path).FromString(std::string(base_path) + "/bin/all/locales");
 	        CefString(&log_file).FromString(std::string(base_path) + "/inexorcef.log");
 	        // TODO: (easylogging) log to debug/verbose
-            // std::cerr << "executable_path: " << executable_path << "\n";
+            std::cerr << "init: cef: setting subprocess executable_path: " << executable_path << "\n";
 	        CefString(&browser_subprocess_path).FromString(std::string(executable_path) + "/cef_subprocess");
         };
 };
