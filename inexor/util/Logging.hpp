e85ece6ae5a90923c69c2c07c6f9263771a50eda
a_teammate <madoe3@web.de>
2016-03-22 19:59:27 +0100

    add main ANSI Color code definitions

--- inexor/util/Logging.hpp
+++ inexor/util/Logging.hpp
@@ -11,17 +11,23 @@
 #include <easylogging++.h>
 #include <iomanip>
 
+/// Function which displayes console text ingame.
 extern void conline(int type, const char *sf);
 
 namespace inexor {
 namespace util {
 
-    // wrapper/callback für das logging-zeug der jedesmal conline( bei bestimmten verbositätsstufen aufruft
+    // ANSI Console colors: everything behind it will be in that particular color.
+    #define COL_GREEN   "\x1b[38;2;64;255;128m" // \x1b[38;2; is the ANSI escape sequence for 24bit foreground color, then the RGB values follow (+ closing 'm').
+    #define COL_BLUE    "\x1b[38;2;96;160;255m"
+    #define COL_YELLOW  "\x1b[38;2;255;192;64m"
+    #define COL_RED     "\x1b[38;2;255;64;64m"
+    #define COL_GREY    "\x1b[38;2;128;128;128m"
+    #define COL_MAGENTA "\x1b[38;2;192;64;192m"
+    #define COL_ORANGE  "\x1b[38;2;255;128;0m"
+    #define COL_WHITE   "\x1b[38;2;255;255;255m"
 
-    // refactoring: conoutf existiert 2x: in server.cpp und in main.cpp -> nur noch 1x in logger.cpp
-
-    // colors: conoutf
-
+    /// Wrapper for easylogger for telling it to call our callback every time something gets logged (so we can display it ingame).
     class InexorConsoleHandler : public el::LogDispatchCallback {
         public:
             InexorConsoleHandler();
