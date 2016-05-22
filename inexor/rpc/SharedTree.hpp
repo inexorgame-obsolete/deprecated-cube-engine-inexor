65ffb3be6fda7be5fd4f13c5d378ce27830183eb
a_teammate <madoe3@web.de>
2016-05-22 19:39:16 +0200

    tree/gluegen: add __REFLECTION_PASS__ macro
    to distinquish between the gluegen pass and the normal build to disable attribute annotation for variables (and let it build for non-clang)

--- inexor/rpc/SharedTree.hpp
+++ inexor/rpc/SharedTree.hpp
@@ -1,11 +1,14 @@
-#ifndef INEXOR_RPC_SHARED_VAR_HEADER
-#define INEXOR_RPC_SHARED_VAR_HEADER
+#pragma once
 
 #include "inexor/util/Observe.hpp"
 
 template<typename T> using SharedVar = inexor::util::Observe<T>;
 
 #define INEXOR_CUSTOM_ANNOTATION(...) __attribute__((annotate( #__VA_ARGS__ )))
-#define INEXOR_SHARED_TREE(...) INEXOR_CUSTOM_ANNOTATION(SharedTree=__VA_ARGS__)
 
+#ifdef __REFLECTION_PASS__
+#define INEXOR_SHARED_TREE(...) INEXOR_CUSTOM_ANNOTATION(SharedTree=__VA_ARGS__)
+#else
+#define INEXOR_SHARED_TREE(...)
 #endif
+
