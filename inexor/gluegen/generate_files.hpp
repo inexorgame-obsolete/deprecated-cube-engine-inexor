d55130f4eb7389df413cb0c8cc1edb49a766e1f4
a_teammate <madoe3@web.de>
2016-06-05 17:20:47 +0200

    [gluegen/fs] add getfilecontents method retrieving a string of the file contents
    todo: move to filesystem module

--- inexor/gluegen/generate_files.hpp
+++ inexor/gluegen/generate_files.hpp
@@ -12,6 +12,7 @@ namespace gluegen {
 extern void render_proto_file(const std::string &out_path, const std::string &template_path, std::vector<ShTreeNode> &tree, TemplateData &tmpldata);
 extern void render_cpp_tree_header(const std::string &out_path, const std::string &template_path, std::vector<ShTreeNode> &tree, TemplateData &tmpldata);
 
+extern const std::string filecontents(const std::string &filename);
 }
 }
 }
