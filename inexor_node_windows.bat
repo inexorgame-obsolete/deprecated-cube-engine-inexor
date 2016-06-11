1a2286d9062c1bbb338014f1144c0ec49d48e364
a_teammate <madoe3@web.de>
2016-06-11 13:36:56 +0200

    npm: always npm update in inexor_node_windows
    since npm update improved a lot in performance (also for empty jobs)

--- inexor_node_windows.bat
+++ inexor_node_windows.bat
@@ -3,8 +3,10 @@
 :: Start the nodejs app as the Inexor scripting backend.
 :: We just call our generic node environment setting batch with the additional commands it shall execute as arguments.
 :: The generic handler sets the PATHs for us.
+setlocal DisableDelayedExpansion
 
-tool\node_windows_generic_handler.bat ^
-echo [STARTING NODE WITH INEXOR BINDING] & ^
-echo [LEAVE THIS WINDOW OPEN WHILE INEXOR IS RUNNING] & ^
+
+call %~dp0tool\node_windows_generic_handler.bat ^
+!NPM_EXECUTEABLE! update & ^
+echo [starting node.js as Inexor scripting backend] & ^
 node app/app.js
