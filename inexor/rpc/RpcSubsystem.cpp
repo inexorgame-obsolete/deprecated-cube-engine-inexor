87c95e72ca3ec54591469618cb506e1e12d17777
a_teammate <madoe3@web.de>
2016-06-12 14:51:46 +0200

    [RPC] add hello world grpc client

--- inexor/rpc/RpcSubsystem.cpp
+++ inexor/rpc/RpcSubsystem.cpp
@@ -1,7 +1,15 @@
-#include <string>
-#include <iostream>
+
 
 #include "inexor/rpc/RpcSubsystem.hpp"
+#include "inexor/util/Logging.hpp"
+#include <memory>
+#include <string>
+
+#include <grpc++/grpc++.h>
+
+#include "inexor/rpc/inexor_service.grpc.pb.h"
+#include "inexor/rpc/inexor_service.pb.h"
+
 
 using namespace std;
 using namespace inexor::util;
@@ -38,6 +46,61 @@ void RpcSubsystem::tick()
 {
     //socket->accept_all();
     //server->ProcessAllCalls();
+}
+
+using grpc::Channel;
+using grpc::ClientContext;
+using grpc::Status;
+using helloworld::HelloRequest;
+using helloworld::HelloReply;
+using helloworld::Greeter;
+
+class GreeterClient {
+public:
+    GreeterClient(std::shared_ptr<Channel> channel)
+        : stub_(Greeter::NewStub(channel)) {}
+
+    // Assambles the client's payload, sends it and presents the response back
+    // from the server.
+    std::string SayHello(const std::string& user) {
+        // Data we are sending to the server.
+        HelloRequest request;
+        request.set_name(user);
+
+        // Container for the data we expect from the server.
+        HelloReply reply;
+
+        // Context for the client. It could be used to convey extra information to
+        // the server and/or tweak certain RPC behaviors.
+        ClientContext context;
+
+        // The actual RPC.
+        Status status = stub_->SayHello(&context, request, &reply);
+
+        // Act upon its status.
+        if (status.ok()) {
+            return reply.message();
+        }
+        else {
+            return "RPC failed";
+        }
+    }
+
+private:
+    std::unique_ptr<Greeter::Stub> stub_;
+};
+
+void testclientrpc()
+{
+    // Instantiate the client. It requires a channel, out of which the actual RPCs
+    // are created. This channel models a connection to an endpoint (in this case,
+    // localhost at port 50051). We indicate that the channel isn't authenticated
+    // (use of InsecureChannelCredentials()).
+    GreeterClient greeter(grpc::CreateChannel("localhost:50051", grpc::InsecureChannelCredentials()));
+    std::string user("world");
+    std::string reply = greeter.SayHello(user);
+    spdlog::get("global")->info() << "Greeter received: " << reply;
+
 }
 
 }
