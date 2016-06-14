c70ecb97337ba961e999bbac1056efce62c45b97
a_teammate <madoe3@web.de>
2016-06-14 19:59:41 +0200

    [batch/windows] start node automatically with inexor
    and call npm update beforehand

--- tool/node_windows_update_npm.bat
+++ tool/node_windows_update_npm.bat
@@ -1,6 +1,8 @@
 @echo off
 :: Install any needed files if necessary.
-:: We just call our generic node environment setting batch with the additional commands it shall execute.
-setlocal DisableDelayedExpansion
 
-call %~dp0node_windows_generic_handler.bat call !NPM_EXECUTEABLE! update
+:: we set those to enable those commands in the generic handler
+set "inexor_update_npm=true"
+set "inexor_start_nodeapp=false"
+
+call %~dp0node_windows_generic_handler.bat
