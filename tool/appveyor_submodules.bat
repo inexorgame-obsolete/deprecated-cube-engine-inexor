c499b65473b9a803730ed2b6582fb214bbc666f1
Croydon <cr0ydon@outlook.com>
2016-07-28 03:54:05 +0200

    This is a quick workaround.

--- tool/appveyor_submodules.bat
+++ tool/appveyor_submodules.bat
@@ -13,14 +13,14 @@ for /f "tokens=2 delims=." %%n in ('git config --file .gitmodules --get-regexp p
     echo %%n
 
     :: fetch the path to each submodule
-    for /f %%p in ('git config --file .gitmodules --get submodule.%%n.path') do set SUBMODULE_PATH=%%p
-    for /f %%u in ('git config --file .gitmodules --get submodule.%%n.url') do set SUBMODULE_URL=%%u
+    for /f %%p in ('git config --file .gitmodules --get submodule.%%n.path') do set "SUBMODULE_PATH=%%p"
+    for /f %%u in ('git config --file .gitmodules --get submodule.%%n.url') do set "SUBMODULE_URL=%%u"
    
     :: get the specific commit we want to download of each submodule
     for /f "tokens=3" %%c in ('git ls-tree %CURRENT_COMMIT% %SUBMODULE_PATH%') do ( 
        echo %%c 
-       set SUBMODULE_URL=%SUBMODULE_URL:.git=%
-       set SUBMODULE_DOWNLOAD=%SUBMODULE_URL%/archive/%%c.zip
+       set "SUBMODULE_URL=%SUBMODULE_URL:.git=%"
+       set "SUBMODULE_DOWNLOAD=%SUBMODULE_URL%/archive/%%c.zip"
        echo %SUBMODULE_DOWNLOAD%
        powershell -Command "Invoke-WebRequest %SUBMODULE_DOWNLOAD% -OutFile build/%%c.zip"
        7z e build/%%c.zip -o %SUBMODULE_PATH%
