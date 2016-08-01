81aadb42e8372f78f1a4b917c7b79a109509e9fb
a_teammate <madoe3@web.de>
2016-08-01 20:29:04 +0200

    [GLUEGEN] make gluegen find almost all SharedVars
    but 150/583 are missing: which ones and why?
    
    note: path is now not prepending a "/" @Hanack

--- inexor/gluegen/parse_sourcecode.cpp
+++ inexor/gluegen/parse_sourcecode.cpp
@@ -12,17 +12,16 @@
 using namespace pugi;
 using namespace boost;
 using namespace inexor::filesystem;
+using namespace std;
 
 
 namespace inexor { namespace rpc { namespace gluegen {
 
 Sharedwo x(Range(0, 2)|CustomPath("inexor/rendering/screen/"));
 
-
-
 bool find_shared_decls(const std::string xml_folder, std::vector<ShTreeNode> &tree)
 {
-    try {
+    //sorting input files:
     std::vector<Path> all_xmls;
     std::vector<Path> cpp_xmls;
     std::vector<Path> class_xmls;
@@ -30,91 +29,65 @@ bool find_shared_decls(const std::string xml_folder, std::vector<ShTreeNode> &tr
     list_files(xml_folder, all_xmls, ".xml");
     for(auto file : all_xmls)
     {
-        //std::cout << "file: " << file.filename() << std::endl;
         if(contains(file.filename().string(), "_8cpp.xml")) cpp_xmls.push_back(file);
         if(contains(file.stem().string(), "class") || contains(file.stem().string(), "struct")) class_xmls.push_back(file);
     }
-    //for(auto file : cpp_xmls)
-    //{
-    //    std::cout << "cpp xmls: " << file.stem() << std::endl;
-    //}
-    //for(auto file : class_xmls)
-    //{
-    //    std::cout << "class xmls: " << file.stem() << std::endl;
-    //}
 
-    xml_document xml;
-    if(!xml.load_file(cpp_xmls.begin()->c_str(), parse_default|parse_trim_pcdata))
+    // parsing cpp-file xmls for shared declarations.
+    //
+    // doxygens AST in cpp files xml is mainly:
+    // doxygen
+    //   compound
+    //     section("func")
+    //       member
+    //       member..
+    //     section("var")
+    //     section("define")..
+    for(auto file : cpp_xmls)
     {
-        std::cout << "XML file representing the AST couldn't be parsed: " << cpp_xmls.begin()->c_str() << std::endl;
-        return false;
-    }
+        std::cout << "processing file: " << file.make_preferred() << std::endl;
 
-    std::cout << cpp_xmls.begin()->filename().string() << std::endl;
-    xml_node compound_xml = xml.child("doxygen").child("compounddef"); //[@kind='file' and @language='C++']");
-    //for(auto it: compound_xml.attributes())
-    //{
-    //    std::cout << "comp: " << it.name() << " = " << it.as_string() << std::endl;
-    //}
-    for(auto section : compound_xml.children("sectiondef"))
-    {
-        std::string attrvalue = section.attribute("kind").value();
-        if(!attrvalue.compare("func"))
+        xml_document xml;
+        if(!xml.load_file(file.make_preferred().c_str(), parse_default|parse_trim_pcdata))
         {
-            for(auto member : section.children("memberdef"))
+            std::cout << "XML file representing the AST couldn't be parsed: " << file << std::endl;
+            continue;
+        }
+
+        xml_node compound_xml = xml.child("doxygen").child("compounddef"); //[@kind='file' and @language='C++']");
+
+
+        // skip files not including our API definition header.
+        bool file_includes_treehpp = false;
+        for(xml_node header : compound_xml.children("includes"))
+            if(!strcmp(header.text().as_string(), "inexor/rpc/SharedTree.hpp")) file_includes_treehpp = true;
+        //if(!file_includes_treehpp)
+        //{
+        //    std::cout << "skipping xml file: " << file.make_preferred() << " (no sharedtree.hpp include found)" << std::endl;
+        //    continue;
+        //}
+
+        std::vector<xml_node> sharedvar_nodes;
+        for(auto section : compound_xml.children("sectiondef"))
+        {
+            if(!strncmp(section.attribute("kind").value(), "func", 4))
             {
-                std::cout << "type: " << member.child("type").text().as_string() << std::endl;
-                std::cout << "name: " << member.child("name").text().as_string() << std::endl;
-                std::cout << "argsstring: " << member.child("argsstring").text().as_string() << std::endl;
-                ShTreeNode *var = new ShTreeNode();
+                for(auto member : section.children("memberdef"))
+                {
+                    if(strstr(member.child("type").text().as_string(), "SharedVar"))
+                        sharedvar_nodes.push_back(member);
+                }
             }
         }
-    }
 
-
-
-    //std::vector<xpath_node> all_variables;
-    //for(auto file : file_xmls)
-    //{
-    //    pugi::xpath_node variablesection = file.node.select_node("sectiondef[@kind='var']");
-    //    pugi::xpath_node_set variables = variablesection.node.select_nodes("memberdef[@kind='var']");
-    //    for(auto var : variables)
-    //    {
-    //        if(var.node().child_value("type") == "SharedVar<int>"); // strcmp
-    //    }
-
-
-    //    pugi::xpath_node_set functions = file.node.select_nodes("sectiondef[@kind='func']");
-    //}
-
-    //for(xpath_node file : file_xmls)
-    //{
-    //    bool hasrightinclude = x.zwei;
-    //    for(xml_node header : file.node().children("includes"))
-    //        if(strcmp(header.text().get(), "inexor/rpc/SharedTree.hpp")) hasrightinclude = true;
-    //    if(!hasrightinclude) continue;
-    //    xpath_node_set 
-    //}
-
-    //std::cout << "Tools:\n";
-
-    //for(pugi::xpath_node_set::const_iterator it = tools.begin(); it != tools.end(); ++it)
-    //{
-    //    pugi::xpath_node node = *it;
-    //    std::cout << node.node().attribute("Filename").value() << "\n";
-    //}
-
-    //pugi::xpath_node build_tool = doc.select_node("//Tool[contains(Description, 'build system')]");
-
-    //if(build_tool)
-    //    std::cout << "Build tool: " << build_tool.node().attribute("Filename").value() << "\n";
-
-
-    }
-    catch(xpath_exception &e)
-    {
-        std::cout << "Fatal: XML Parsing Exception thrown (XPATH): " << std::endl
-                  << e.what() << std::endl;
+        // handle shared variable declarations.
+        for(auto var : sharedvar_nodes)
+        {
+            const char *type = var.child("type").text().as_string();
+            const char *name = var.child("name").text().as_string();
+            std::cout << "type: " << type << " name: " << name << " argsstring: " << var.child("argsstring").text().as_string() << std::endl;
+            tree.push_back(ShTreeNode(type, name));
+        }
     }
     return true;
 }
