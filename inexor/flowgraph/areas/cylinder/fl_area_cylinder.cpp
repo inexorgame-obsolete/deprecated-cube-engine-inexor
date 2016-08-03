ade6a4b5e4441b994b6148f7a5037725e8d3d4e2
IAmNotHanni <cmdfreak@web.de>
2016-08-03 23:24:17 +0200

    Added cylinder node (no rendering yet).

---
+++ inexor/flowgraph/areas/cylinder/fl_area_cylinder.cpp
@@ -0,0 +1,54 @@
+#include "inexor/flowgraph/areas/cylinder/fl_area_cylinder.hpp"
+
+namespace inexor {
+namespace vscript {
+
+    CCylinderAreaNode::CCylinderAreaNode(vec position, float radius, float height, const char* name, const char* comment = "")
+    {
+        type = NODE_TYPE_AREA_BOX;
+        pos = position;
+        node_name = name;
+        node_comment = comment;
+        cylinder_height = height;
+        cylinder_radius = radius;
+    }
+
+    CCylinderAreaNode::~CCylinderAreaNode()
+    {
+    }
+
+    bool CCylinderAreaNode::collide(vec p)
+    {
+        return true;
+    }
+
+    void CCylinderAreaNode::render_additional()
+    {
+        gle::color(vec::hexcolor(VSCRIPT_AREA));
+
+        glBegin(GL_LINE_LOOP);
+        loopi(render_detail_level)
+        {
+            vec tmp = vec(pos).rotate(2 * M_PI*i / render_detail_level, vec(0,1,0));
+            glVertex3f(tmp.x, tmp.y, tmp.z);
+        }
+        glEnd();
+    }
+
+    bool CCylinderAreaNode::OnLinkAsChildNodeAttempt(CScriptNode* parent)
+    {
+        conoutf(CON_DEBUG, "[3DVS-cylinderarea] a cone area can't run any code so it can't be linked as child!");
+        return false;
+    }
+
+    bool CCylinderAreaNode::OnLinkAsParentNodeAttempt(CScriptNode* child)
+    {
+        if(child->type != NODE_TYPE_EVENT)
+        {
+            conoutf(CON_DEBUG, "[3DVS-cylinderarea] a cone can only be linked as parent of an event node!");
+        }
+        return true;
+    }
+
+};
+};
---
+++ inexor/flowgraph/areas/cylinder/fl_area_cylinder.hpp
@@ -0,0 +1,38 @@
+/// @file fl_area_cylinder.hpp
+/// @author Johannes Schneider
+/// @brief cylinders are defined by position, radius, and height
+
+// TODO: make cylinders rotatable in 3 dimensions
+
+#ifndef INEXOR_VSCRIPT_AREA_CYLINDER_HEADER
+#define INEXOR_VSCRIPT_AREA_CYLINDER_HEADER
+
+#include "inexor/flowgraph/nodebase/fl_base.hpp"
+
+namespace inexor {
+namespace vscript {
+
+    class CCylinderAreaNode : public CScriptNode
+    {
+        protected:
+            
+            float cylinder_height;
+            float cylinder_radius;
+            const int render_detail_level = 20;
+
+        public:
+
+            CCylinderAreaNode(vec, float, float, const char*, const char*);
+            ~CCylinderAreaNode();
+            
+            bool collide(vec p);
+            void render_additional();
+
+            bool OnLinkAsChildNodeAttempt(CScriptNode* parent);
+            bool OnLinkAsParentNodeAttempt(CScriptNode* child);
+    };
+
+};
+};
+
+#endif
