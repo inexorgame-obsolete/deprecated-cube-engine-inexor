542b4a25c3e8620a06cfa18ce8230135d33b4335
aschaeffer <hanack@nooblounge.net>
2016-09-18 15:25:00 +0200

    Very very basic CEF with two executables
    Conflicts:
    	inexor/ui/CefSubsystem.cpp
    	inexor/ui/cefcontextmanager.cpp
    	inexor/ui/cefcontextprovider.cpp
    	inexor/ui/cefcontextprovider.hpp
    	inexor/ui/cefframe.cpp
    	inexor/ui/cefframe.hpp
    	inexor/ui/cefkeyboard.cpp
    	inexor/ui/ceflayer.cpp
    	inexor/ui/ceflayer.hpp
    	inexor/ui/ceflayermanager.cpp

--- inexor/cef_subprocess/main.cpp
+++ inexor/cef_subprocess/main.cpp
@@ -7,7 +7,7 @@ int main(int argc, char **argv) {
     setlocale(LC_ALL, "en_US.utf8");
     std::cerr << "init: cef: cef_subprocess\n";
 
-    std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+    std::this_thread::sleep_for(std::chrono::milliseconds(5000));
 
 #ifdef WIN32
     CefMainArgs main_args(GetModuleHandle(NULL));
