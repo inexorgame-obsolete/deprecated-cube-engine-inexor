e72d1ef71d3cc23ed026afb9ae707d32daf8e732
a_teammate <madoe3@web.de>
2016-12-14 19:04:00 +0100

    Todo merge: add toplevel dependencies.py

---
+++ dependencies.py
@@ -0,0 +1,13 @@
+requires = (("Kainjow_Mustache/2.0@a_teammate/stable"),
+            ("pugixml/1.7@a_teammate/testing"),
+            ("Boost/1.60.0@lasote/stable"), # These are requirements of gluegen
+            ("RapidJSON/1.0.2@SamuelMarks/stable"),
+            ("zlib/1.2.8@lasote/stable"),
+            ("gtest/1.7.0@lasote/stable"),
+            ("ENet/1.3.13@a_teammate/stable"),
+            ("spdlog/0.10.0@memsharded/stable"),
+            ("SDL2/2.0.5@lasote/stable"),
+            ("SDL2_image/2.0.1@lasote/stable"),
+            ("CEF/3.2704.1424.gc3f0a5b@a_teammate/testing"),
+            ("Protobuf/3.1.0@a_teammate/stable"),
+            ("gRPC/1.1.0-dev@a_teammate/stable"))
