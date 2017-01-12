4eda7c60daad329c391b03c4f04d1d6d0faa3bd5
Andreas Schaeffer <hanack@nooblounge.net>
2017-01-12 18:10:32 +0100

    Update the scheme handler (#329)

--- tool/install_desktop_integration.sh
+++ tool/install_desktop_integration.sh
@@ -8,3 +8,5 @@ sudo cp dist/linux/inexor.desktop /usr/share/applications
 sudo sed -i -- "s+flex+${flex_path}+g" /usr/share/applications/inexor.desktop
 wget https://raw.githubusercontent.com/inexor-game/artwork/master/logo/logo_thick.svg -O /tmp/inexor.svg
 sudo cp /tmp/inexor.svg /usr/share/pixmaps
+
+sudo update-desktop-database
