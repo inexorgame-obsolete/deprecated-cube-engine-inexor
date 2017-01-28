ae88d4c299e1b8f8240564d1ab251c823755a48b
a_teammate <madoe3@web.de>
2017-01-28 20:51:41 +0100

    [GRPC] use first main argv as port for the gRPC server

--- inexor/rpc/RpcSubsystem.hpp
+++ inexor/rpc/RpcSubsystem.hpp
@@ -22,17 +22,30 @@ extern const std::unordered_map<int64, int> index_to_type_map;
 template<typename MSG_TYPE, typename ASYNC_SERVICE_TYPE>
 class RpcSubsystem : public inexor::util::Subsystem
 {
-    RpcServer<MSG_TYPE, ASYNC_SERVICE_TYPE> serv;
+    RpcServer<MSG_TYPE, ASYNC_SERVICE_TYPE> *serv = nullptr;
 public:
-    RpcSubsystem() : serv("0.0.0.0:50051")
+    RpcSubsystem() { }
+
+    ~RpcSubsystem()
     {
-        spdlog::get("global")->info("RPC server listening on {0}", serv.server_address);
+        spdlog::get("global")->info("RPC server stopped ({0})", serv->server_address);
+        delete serv;
+    }
+
+   // RpcSubsystem() : serv("0.0.0.0:50051")
+    virtual void initialize(int argc, char **argv) override
+    {
+        if(argc < 2) return;
+        std::string port = argv[1];
+        std::string full_address = "0.0.0.0:"+ port;
+        serv = new RpcServer<MSG_TYPE, ASYNC_SERVICE_TYPE>(full_address.c_str());
+        spdlog::get("global")->info("RPC server listening on {0}", serv->server_address);
         set_on_change_functions();
     }
 
-    void tick() override
+    virtual void tick() override
     {
-        serv.process_queue();
+        serv->process_queue();
     }
 };
 
