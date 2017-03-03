a1d17dca735f5f12609b82469c4bad835ae8ee92
a_teammate <madoe3@web.de>
2017-03-03 22:08:12 +0100

    [GLUEGEN] output if template is valid or not

--- inexor/gluegen/generate_files.cpp
+++ inexor/gluegen/generate_files.cpp
@@ -17,10 +17,14 @@ void render_proto_file(const std::string &out_path, const std::string &template_
     const std::string template_file_contents(filecontents(template_path));
 
     MustacheTemplate tmpl{template_file_contents.c_str()};
+    if(!tmpl.isValid())
+        std::cout << "Template file " << template_path << " malformatted: " << tmpl.errorMessage() << std::endl;
+
     std::ofstream sink{out_path, std::ofstream::trunc};
     tmpl.render(tmpldata, [&sink](const std::string& str) {
         sink << str;
     });
+    std::cout << "Rendering Proto file completed." << std::endl;
 }
 
 void render_cpp_tree_data(const std::string &out_path, const std::string &template_path, TemplateData &tmpldata)
@@ -28,10 +32,14 @@ void render_cpp_tree_data(const std::string &out_path, const std::string &templa
     const std::string template_file_contents(filecontents(template_path));
 
     MustacheTemplate tmpl{template_file_contents.c_str()};
+    if(!tmpl.isValid())
+        std::cout << "Template file " << template_path << " malformatted: " << tmpl.errorMessage() << std::endl;
+
     std::ofstream sink{out_path, std::ofstream::trunc};
     tmpl.render(tmpldata, [&sink](const std::string& str) {
         sink << str;
     });
+    std::cout << "Rendering C++ GlueGen file completed." << std::endl;
 }
 
 
