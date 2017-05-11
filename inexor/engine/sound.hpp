43a0113bfb7d287ea43013dddf2a1954c8b10342
a_teammate <madoe3@web.de>
2017-05-11 20:47:49 +0200

    refactor(sound): move sound slot enum to sounds.hpp

--- inexor/engine/sound.hpp
+++ inexor/engine/sound.hpp
@@ -1,4 +1,43 @@
 #pragma once
+#include "inexor/network/SharedTree.hpp"
+/// hardcoded sound slots (to reference them in code)
+/// sound files get assigned to these slots in sounds.cfg
+enum
+{
+    S_JUMP = 0, S_LAND, S_RIFLE, S_PUNCH1, S_SG, S_CG,
+    S_RLFIRE, S_RLHIT, S_WEAPLOAD, S_ITEMAMMO, S_ITEMHEALTH,
+    S_ITEMARMOUR, S_ITEMPUP, S_ITEMSPAWN, S_TELEPORT, S_NOAMMO, S_PUPOUT,
+    S_PAIN1, S_PAIN2, S_PAIN3, S_PAIN4, S_PAIN5, S_PAIN6,
+    S_DIE1, S_DIE2,
+    S_FLAUNCH, S_FEXPLODE,
+    S_SPLASH1, S_SPLASH2,
+    S_GRUNT1, S_GRUNT2, S_RUMBLE,
+    S_PAINO,
+    S_PAINR, S_DEATHR,
+    S_PAINE, S_DEATHE,
+    S_PAINS, S_DEATHS,
+    S_PAINB, S_DEATHB,
+    S_PAINP, S_PIGGR2,
+    S_PAINH, S_DEATHH,
+    S_PAIND, S_DEATHD,
+    S_PIGR1, S_ICEBALL, S_SLIMEBALL,
+    S_JUMPPAD, S_PISTOL,
+    S_V_BASECAP, S_V_BASELOST,
+    S_V_FIGHT,
+    S_V_BOOST, S_V_BOOST10,
+    S_V_QUAD, S_V_QUAD10,
+    S_V_RESPAWNPOINT,
+    S_FLAGPICKUP,
+    S_FLAGDROP,
+    S_FLAGRETURN,
+    S_FLAGSCORE,
+    S_FLAGRESET,
+    S_BURN,
+    S_CHAINSAW_ATTACK,
+    S_CHAINSAW_IDLE,
+    S_HIT,
+    S_FLAGFAIL
+};
 
 namespace inexor {
 namespace sound {
--- inexor/fpsgame/game.hpp
+++ inexor/fpsgame/game.hpp
@@ -220,47 +220,6 @@ static const char * const mastermodecolors[] = { "",    COL_GREEN,  COL_YELLOW, 
 static const char * const mastermodeicons[] =  { "server", "server", "serverlock", "serverlock", "serverpriv", "serverpriv" };
 
 
-/// ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-/// sound list and description of administrative levels
-
-/// hardcoded sounds, defined in sounds.cfg
-enum
-{
-    S_JUMP = 0, S_LAND, S_RIFLE, S_PUNCH1, S_SG, S_CG,
-    S_RLFIRE, S_RLHIT, S_WEAPLOAD, S_ITEMAMMO, S_ITEMHEALTH,
-    S_ITEMARMOUR, S_ITEMPUP, S_ITEMSPAWN, S_TELEPORT, S_NOAMMO, S_PUPOUT,
-    S_PAIN1, S_PAIN2, S_PAIN3, S_PAIN4, S_PAIN5, S_PAIN6,
-    S_DIE1, S_DIE2,
-    S_FLAUNCH, S_FEXPLODE,
-    S_SPLASH1, S_SPLASH2,
-    S_GRUNT1, S_GRUNT2, S_RUMBLE,
-    S_PAINO,
-    S_PAINR, S_DEATHR,
-    S_PAINE, S_DEATHE,
-    S_PAINS, S_DEATHS,
-    S_PAINB, S_DEATHB,
-    S_PAINP, S_PIGGR2,
-    S_PAINH, S_DEATHH,
-    S_PAIND, S_DEATHD,
-    S_PIGR1, S_ICEBALL, S_SLIMEBALL,
-    S_JUMPPAD, S_PISTOL,
-    S_V_BASECAP, S_V_BASELOST,
-    S_V_FIGHT,
-    S_V_BOOST, S_V_BOOST10,
-    S_V_QUAD, S_V_QUAD10,
-    S_V_RESPAWNPOINT,
-    S_FLAGPICKUP,
-    S_FLAGDROP,
-    S_FLAGRETURN,
-    S_FLAGSCORE,
-    S_FLAGRESET,
-    S_BURN,
-    S_CHAINSAW_ATTACK,
-    S_CHAINSAW_IDLE,
-    S_HIT,
-    S_FLAGFAIL
-};
-
 /// important teamspecific declarations
 #define MAXTEAMS 128
 #define MAXNAMELEN 15  /// max player name length
