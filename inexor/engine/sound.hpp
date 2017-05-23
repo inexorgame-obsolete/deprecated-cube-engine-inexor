17e11d9c0d01bccf9f3a6a44f8cdb3ea3ffb1c1c
a_teammate <madoe3@web.de>
2017-05-23 11:59:44 +0200

    refactor(header): move inengine sound stuff to sound.hpp

--- inexor/engine/sound.hpp
+++ inexor/engine/sound.hpp
@@ -1,5 +1,8 @@
 #pragma once
 #include "inexor/network/SharedTree.hpp"
+#include "inexor/shared/geom.hpp"
+#include "inexor/shared/ents.hpp"
+
 /// hardcoded sound slots (to reference them in code)
 /// sound files get assigned to these slots in sounds.cfg
 enum
@@ -45,5 +48,13 @@ namespace sound {
 extern bool nosound;
 
 extern SharedVar<int> soundchans, soundfreq, soundbufferlen;
+
+extern int playsound(int n, const vec *loc = NULL, extentity *ent = NULL, int flags = 0, int loops = 0, int fade = 0, int chanid = -1, int radius = 0, int expire = -1);
+extern int playsoundname(const char *s, const vec *loc = NULL, int vol = 0, int flags = 0, int loops = 0, int fade = 0, int chanid = -1, int radius = 0, int expire = -1);
+extern void preloadsound(int n);
+extern void preloadmapsound(int n);
+extern bool stopsound(int n, int chanid, int fade = 0);
+extern void stopsounds();
+extern void initsound();
 
 } } // ns inexor::sound
