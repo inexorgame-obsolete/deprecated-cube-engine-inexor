0d5b4ae3c5cffcb6ef48c80e1577edec8aeca69f
a_teammate <madoe3@web.de>
2017-06-03 23:36:34 +0200

    [PM] outsource dependencies to dependencies.py again

--- conanfile.py
+++ conanfile.py
@@ -1,35 +1,14 @@
 from conans import ConanFile, CMake
-import os, multiprocessing
+import os, dependencies, multiprocessing
 
 class InexorConan(ConanFile):
     license = "ZLIB"
     url = "https://github.com/inexor-game/code.git"
     settings = "os", "compiler", "build_type", "arch"
-    requires = (("InexorGlueGen/0.6.0alpha@inexorgame/testing"),
-                ("Protobuf/3.1.0@inexorgame/stable"),
-                ("gRPC/1.1.0-dev@inexorgame/stable"),
-                ("doxygen/1.8.13@inexorgame/testing"),
-                ("Boost/1.60.0@lasote/stable"),
-                ("RapidJSON/1.0.2@inexorgame/stable"),
-                ("zlib/1.2.8@lasote/stable"),
-                ("gtest/1.7.0@lasote/stable"),
-                ("ENet/1.3.13@inexorgame/stable"),
-                ("spdlog/0.10.0@memsharded/stable"),
-                ("SDL2/2.0.5@lasote/stable"),
-                ("SDL2_image/2.0.1@lasote/stable"),
-                ("CEF/3.2704.1424.gc3f0a5b@a_teammate/testing")
-            )
 
+    requires = dependencies.requires
     generators = "cmake"
-    default_options = '''
-      zlib:shared=False
-      gtest:shared=False
-      ENet:shared=False
-      Boost:shared=False
-      SDL2:shared=False
-      SDL2_image:shared=False
-      CEF:use_sandbox=False
-    '''
+    default_options = dependencies.options
 
     def configure(self):
         if self.settings.compiler == "gcc":
