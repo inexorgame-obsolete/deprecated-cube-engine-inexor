08f9319c0f11f6b0a12490aa0daa29077c24f4b9
a_teammate <madoe3@web.de>
2017-06-15 16:03:32 +0200

    refactor(filesystem): remove legacy streams SDL dependency

--- inexor/shared/stream.cpp
+++ inexor/shared/stream.cpp
@@ -296,43 +296,6 @@ int listfiles(const char *dir, const char *ext, vector<char *> &files)
     return dirs;
 }
 
-#ifndef STANDALONE
-static Sint64 rwopsseek(SDL_RWops *rw, Sint64 pos, int whence)
-{
-    stream *f = (stream *)rw->hidden.unknown.data1;
-    if((!pos && whence==SEEK_CUR) || f->seek(pos, whence)) return (int)f->tell();
-    return -1;
-}
-
-static size_t rwopsread(SDL_RWops *rw, void *buf, size_t size, size_t nmemb)
-{
-    stream *f = (stream *)rw->hidden.unknown.data1;
-    return f->read(buf, size*nmemb)/size;
-}
-
-static size_t rwopswrite(SDL_RWops *rw, const void *buf, size_t size, size_t nmemb)
-{
-    stream *f = (stream *)rw->hidden.unknown.data1;
-    return f->write(buf, size*nmemb)/size;
-}
-
-static int rwopsclose(SDL_RWops *rw)
-{
-    return 0;
-}
-
-SDL_RWops *stream::rwops()
-{
-    SDL_RWops *rw = SDL_AllocRW();
-    if(!rw) return NULL;
-    rw->hidden.unknown.data1 = this;
-    rw->seek = rwopsseek;
-    rw->read = rwopsread;
-    rw->write = rwopswrite;
-    rw->close = rwopsclose;
-    return rw;
-}
-#endif
 
 stream::offset stream::size()
 {
