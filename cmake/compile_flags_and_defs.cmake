56d65fc2b84940686d3cf9eaddb4f997f52991d7
Croydon <cr0ydon@outlook.com>
2017-12-01 17:07:49 +0100

    upgrade(ci): Newer compilers, Conan package uploading...
    and more. See pull request #468 for additional information.

--- cmake/compile_flags_and_defs.cmake
+++ cmake/compile_flags_and_defs.cmake
@@ -2,7 +2,7 @@
 ## Note: This file needs to be included **after** platform_detection.cmake.
 
 
-## GCC and CLang 
+## GCC and CLang
 list(APPEND GNU_OR_CLANG_COMPILER_FLAGS
   -fomit-frame-pointer            # Don't keep the frame pointer for functions that don't need one
   -fno-strict-aliasing            # Avoid assumptions regarding non-aliasing of objects of different types
@@ -10,7 +10,7 @@ list(APPEND GNU_OR_CLANG_COMPILER_FLAGS
   -pipe                           # Use pipes rather than temporary files for communication between build stages
   -Wall                           # Enable all warnings
   -Wno-switch                     # Execpt Warnings for missing switch cases
-  -Wno-deprecated-declarations    # 
+  -Wno-deprecated-declarations    #
   -Wno-missing-braces             #
   -fdiagnostics-show-option       # Show Warning IDs
   -g                              # Generate debug information
@@ -170,9 +170,8 @@ if(OS_WINDOWS)
   add_definitions(-DWIN32 -D_WIN32 -D_WINDOWS -DWINDOWS -DNOMINMAX -D_WIN32_WINNT=0x0600 -D_MATH_DEFINES_DEFINED -DWIN32_LEAN_AND_MEAN )
   if(X64)
     add_definitions(-DWIN64)
-  endif()	
+  endif()
 endif()
 
 get_directory_property(DEFINITIONS              COMPILE_DEFINITIONS)
 message(STATUS "Platform definitions:           ${DEFINITIONS}")
-
--- dependencies.py
+++ dependencies.py
@@ -1,6 +1,6 @@
 requires = (("InexorGlueGen/0.6.0alpha@inexorgame/stable"),
             ("Protobuf/3.1.0@inexorgame/stable"),
-            ("gRPC/1.1.0-dev@inexorgame/stable"),
+            ("gRPC/1.1.0@inexorgame/stable"),
             ("doxygen/1.8.13@inexorgame/stable"),
             ("Boost/1.64.0@lasote/stable"),
             ("RapidJSON/1.0.2@inexorgame/stable"),
