10e9dfa1fa6bdbce8a5e0e6e4505a8da45e20963
Croydon <cr0ydon@outlook.com>
2017-12-10 05:44:36 +0100

    enhancement(ci): First steps for adding macOS

---
+++ tool/travis_macos.sh
@@ -0,0 +1,202 @@
+#! /bin/bash
+#
+## UTILITY FUNCTIONS #######################################
+
+# Check if a string contains something
+contains() {
+  test "`subrm "$1" "$2"`" != "$1"
+}
+
+# Remove a substring by name
+subrm() {
+  echo "${1#*$2}"
+}
+
+mkcd() {
+  mkdir -pv "$1"
+  cd "$1"
+}
+
+# Check whether this travis job runs for the main repository (inexorgame/inexor-core)
+is_main_repo() {
+  test "${TRAVIS_REPO_SLUG}" = "${main_repo}"
+}
+
+# Check whether this build is for a pull request
+is_pull_request() {
+  test "${TRAVIS_PULL_REQUEST}" != false
+}
+
+# Check whether this is a pull request, wants to merge a
+# branch within the main repo into the main repo.
+#
+# E.g. Merge inexor-game/code: karo/unittesting
+#      into  inexor-game/code: master
+self_pull_request() {
+  is_pull_request && is_main_repo
+}
+
+# Check whether this is a pull request, that pulls a branch
+# from a different repo.
+external_pull_request() {
+  if is_main_repo; then
+    false
+  else
+    is_pull_request
+  fi
+}
+
+install_dependencies() {
+  pip install conan
+}
+
+## INSTALLATION ROUTINES ###################################
+
+build() {
+  (
+    mkcd "/tmp/inexor-build"
+
+    conan --version
+
+    conan remote add inexor https://api.bintray.com/conan/inexorgame/inexor-conan --insert
+    ## https://bintray.com/conan/conan-center is second
+    conan remote add bincrafers https://api.bintray.com/conan/bincrafters/public-conan --insert 3
+    conan remote add community https://api.bintray.com/conan/conan-community/conan --insert 4
+
+    conan remote list
+
+    conan info "$gitroot"
+
+    if test "$NIGHTLY" = conan; then
+      echo "execut conan install "$gitroot" --scope build_all=1 --build -s compiler.libcxx=libstdc++11"
+      conan install "$gitroot" --scope build_all=1 --build -s compiler.libcxx="libstdc++11"
+    else
+      echo "execut conan install "$gitroot" --scope build_all=1 --scope create_package=1 --build=missing -s compiler.libcxx=libstdc++11"
+      conan install "$gitroot" --scope build_all=1 --scope create_package=1 --build=missing -s compiler.libcxx="libstdc++11"
+    fi
+
+    conan build "$gitroot"
+
+    # Moving the CPack package to the /inexor directory, so we are able to access it from outside of Docker
+    local tempdir="/tmp/inexor-build/"
+    local zipname="inexor-core-${last_tag}-Linux64.zip"
+    local outputdir="/inexor/build/cpack/"
+    mkdir -pv ${outputdir}
+    mv -f -v -u "${tempdir}${zipname}" "${outputdir}"
+  )
+}
+
+run_tests() {
+  if contains "$TARGET" macos; then
+    "${bin}/unit_tests.exe"
+  else
+    echo >&2 "ERROR: UNKNOWN TRAVIS TARGET: ${TARGET}"
+    exit 23
+  fi
+}
+
+
+# ATTENTION:
+# Please USE the following naming format for any files uploaded to our distribution server
+# <BRANCHNAME>-<BUILDNUMBER>-<TARGET_NAME>.EXTENSION
+# where <PACKAGENAME> is NOT CONTAINING any -
+# correct: master-1043.2-linux_gcc.txt
+# correct: refactor-992.2-apidoc.hip
+# exception: master-latest-<TARGET_NAME>.zip
+# wrong: ...-linux_gcc-1043.2.zip
+
+## UPLOADING BUILDS AND THE APIDOC #################
+
+# upload remote_path local_path [local_paths]...
+#
+# Upload one or more files to our nightly or dependencies server
+# Variables are defined on the Travis website
+upload() {
+  # Fix an issue where upload directory gets specified by subsequent upload() calls
+  ncftpput -R -v -u "$FTP_USER" -p "$FTP_PASSWORD" "$FTP_DOMAIN" /macos/ "$@" || true
+}
+
+
+## increment the version number based on the last tag.
+incremented_version()
+{
+  local major_version=`echo -e "${last_tag}" | sed "s/^\(.*\)\\.[0-9]\+\.[0-9]\+.*$/\1/"`
+  local minor_version=`echo -e "${last_tag}" | sed "s/^[0-9]\+\.\(.*\)\.[0-9]\+.*$/\1/"`
+  local patch_version=`echo -e "${last_tag}" | sed "s/^[0-9]\+\.[0-9]\+\.\(.[0-9]*\).*$/\1/"`
+
+  local new_patch_version=$((patch_version+1))
+  local new_version="$major_version.$minor_version.$new_patch_version@alpha"
+  echo $new_version
+}
+
+# Upload nightly
+target_after_success() {
+  if test "$NIGHTLY" = conan; then
+    # Upload all conan packages to our Bintray repository
+    conan user -p "${NIGHTLY_PASSWORD}" -r inexor "${NIGHTLY_USER}"
+    set -f
+    conan upload --all --force -r inexor --retry 3 --retry_wait 10 --confirm "*stable*"
+    set +f
+  fi
+}
+
+## MAIN ####################################################
+
+# this makes the entire script fail if one commands fail
+set -e
+
+script="$0"
+tool="`dirname "$0"`"
+code="${tool}/.."
+bin="${code}/bin"
+
+export commit=TRAVIS_COMMIT
+export branch=TRAVIS_BRANCH # The branch we're on
+export jobno=TRAVIS_JOB_NUMBER # The job number
+
+# Name of this build
+export build="$(echo "${branch}-${jobno}" | sed 's#/#-#g')-${TARGET}"
+export main_repo="inexorgame/inexor-core"
+
+
+if [ -z "$2" ]; then
+  export gitroot="."
+else
+  # this makes it possible to run this script successfull
+  # even if doesn't get called from the root directory
+  # of THIS repository
+  # required to make inexor-game/ci-prebuilds working
+  export gitroot="./$2"
+fi
+
+self_pull_request && {
+  echo >&2 -e "Skipping build, because this is a pull " \
+    "request with a branch in the main repo.\n"         \
+    "This means, there should already be a CI job for " \
+    "this branch. No need to do things twice."
+  exit 0
+}
+
+cd "$gitroot"
+
+# Tags do not get fetched from travis usually.
+git fetch origin 'refs/tags/*:refs/tags/*'
+export last_tag=`git describe --tags $(git rev-list --tags --max-count=1)`
+
+# The queue is:
+# a tag gets created on push to the master branch (using travis), then we push the
+# tag to github and that push triggers travis again (which uploads the release packages)
+
+# We use the last tag as version for the package creation
+export INEXOR_VERSION=${last_tag}
+# need_new_tag && {
+  # If we want a new tag
+  # We use the last tag of the master branch + 1.
+  # export INEXOR_VERSION=$(incremented_version) // FIXME: If we want to upload packages one day directly #544
+# }
+
+# main entry point
+install_dependencies
+build
+run_tests
+target_after_success
