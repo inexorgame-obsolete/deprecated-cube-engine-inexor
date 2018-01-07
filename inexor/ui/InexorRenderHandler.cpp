prakharcipher/inexor-core

refactor(log): rename default log to std log
default is a reserved keyword

inexor/ui/InexorRenderHandler.cpp
@@ -44,7 +44,7 @@ void InexorRenderHandler::Initialize()
     if (initialized)
         return;
 
-    Log.default->debug("InexorRenderHandler initializing...");
+    Log.std->debug("InexorRenderHandler initializing...");
 
     // glHint(GL_POLYGON_SMOOTH_HINT, GL_NICEST); VERIFY_NO_ERROR;
 
@@ -67,7 +67,7 @@ void InexorRenderHandler::Initialize()
 
     setlocale(LC_ALL, "en_US.utf8");
 
-    Log.default->debug("InexorRenderHandler initialized!");
+    Log.std->debug("InexorRenderHandler initialized!");
 
     initialized = true;
 }
@@ -125,17 +125,17 @@ void InexorRenderHandler::ClearPopupRects()
 bool InexorRenderHandler::GetViewRect(CefRefPtr<CefBrowser> browser, CefRect &rect)
 {
     rect = CefRect(view_x, view_y, view_width, view_height);
-    Log.default->debug("GetViewRect: ({0}x{1} at {2}, {3})", view_width, view_height, view_x, view_y);
+    Log.std->debug("GetViewRect: ({0}x{1} at {2}, {3})", view_width, view_height, view_x, view_y);
     return true;
 }
 
 bool InexorRenderHandler::SetViewRect(int view_x, int view_y, int view_width, int view_height)
 {
     if (initialized) {
-        // Log.default->info() << "SetViewRect: (" << view_x << ", " << view_y << ", " << view_width << ", " << view_height << ")\n";
+        // Log.std->info() << "SetViewRect: (" << view_x << ", " << view_y << ", " << view_width << ", " << view_height << ")\n";
         int view_width2 = (view_width / 32) * 32;
         int view_height2 = (view_height / 32) * 32;
-        Log.default->debug("SetViewRect: ({0}x{1} at {2}, {3})", view_width2, view_height2, view_x, view_y);
+        Log.std->debug("SetViewRect: ({0}x{1} at {2}, {3})", view_width2, view_height2, view_x, view_y);
 
         bool success = this->view_x != view_x || this->view_y != view_y || this->view_width != view_width2 || this->view_height != view_height2;
         this->view_x = view_x;
@@ -145,7 +145,7 @@ bool InexorRenderHandler::SetViewRect(int view_x, int view_y, int view_width, in
         this->texture_initialized = false;
         return success;
     } else {
-        Log.default->debug("SetViewRect DEFERRED: ({0}x{1} at {2}, {3})", view_width, view_height, view_x, view_y);
+        Log.std->debug("SetViewRect DEFERRED: ({0}x{1} at {2}, {3})", view_width, view_height, view_x, view_y);
 
         this->_view_x = view_x;
         this->_view_y = view_y;
@@ -164,14 +164,14 @@ void InexorRenderHandler::OnPaint(
 ) {
     if (!initialized) {
         if (CefCurrentlyOn(TID_UI)) {
-            Log.default->debug("OnPaint:Initialize ({}x{})", width, height);
-            Log.default->debug("OnPaint:BeforeInitialization ({0}x{1} at {2}, {3})", view_width, view_height, view_x, view_y);
+            Log.std->debug("OnPaint:Initialize ({}x{})", width, height);
+            Log.std->debug("OnPaint:BeforeInitialization ({0}x{1} at {2}, {3})", view_width, view_height, view_x, view_y);
             Initialize();
             browser->GetHost()->WasResized();
-            Log.default->debug("OnPaint:AfterInitialization ({0}x{1} at {2}, {3})", view_width, view_height, view_x, view_y);
+            Log.std->debug("OnPaint:AfterInitialization ({0}x{1} at {2}, {3})", view_width, view_height, view_x, view_y);
             return;
         } else {
-            Log.default->debug("InexorRenderHandler::OnPaint() Wrong thread!");
+            Log.std->debug("InexorRenderHandler::OnPaint() Wrong thread!");
             return;
         }
     }
@@ -200,12 +200,12 @@ void InexorRenderHandler::OnPaint(
         int old_height = view_height;
         // view_width = width;
         // view_height = height;
-        Log.default->debug("OnPaint:X ({}x{})", width, height);
+        Log.std->debug("OnPaint:X ({}x{})", width, height);
         glPixelStorei(GL_UNPACK_ROW_LENGTH, view_width); VERIFY_NO_ERROR;
         // if (!texture_initialized || old_width != view_width || old_height != view_height || (dirtyRects.size() == 1 && dirtyRects[0] == CefRect(0, 0, view_width, view_height))) {
         if (!texture_initialized || old_width != view_width || old_height != view_height) {
             // Update/resize the whole texture.
-            Log.default->debug("OnPaint: Full Resize ({}x{})", width, height);
+            Log.std->debug("OnPaint: Full Resize ({}x{})", width, height);
         	glPixelStorei(GL_UNPACK_SKIP_PIXELS, 0); VERIFY_NO_ERROR;
         	glPixelStorei(GL_UNPACK_SKIP_ROWS, 0); VERIFY_NO_ERROR;
         	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, view_width, view_height, 0, GL_BGRA, GL_UNSIGNED_INT_8_8_8_8_REV, buffer); VERIFY_NO_ERROR;
@@ -228,7 +228,7 @@ void InexorRenderHandler::OnPaint(
         int skip_rows = 0, y = popup_rect.y;
         int w = width;
         int h = height;
-        Log.default->debug("OnPaint:Y ({}x{})", width, height);
+        Log.std->debug("OnPaint:Y ({}x{})", width, height);
 
         // Adjust the popup to fit inside the view.
         if (x < 0) {

inexor/ui/InexorSettings.hpp
@@ -41,7 +41,7 @@ class InexorSettings : public CefSettings
         CefString(&resources_dir_path).FromString(executable_path);
         CefString(&locales_dir_path).FromString(executable_path + "/locales");
         CefString(&log_file).FromString(std::string(base_path) + "inexorcef.log"); // TODO: make adaptable.
-        Log.default->info("init: cef: setting subprocess executable_path: {}", executable_path);
+        Log.std->info("init: cef: setting subprocess executable_path: {}", executable_path);
         CefString(&browser_subprocess_path).FromString(executable_path + "/cef_subprocess");
     }
 

inexor/ui/context/InexorContextProvider.cpp
@@ -11,7 +11,7 @@ CefRefPtr<CefV8Value> InexorContextProvider::GetContext()
     if (!context.get())
     {
         // This has to be happen when the context is initialized
-        Log.default->debug("init: cef: context \"{}\"", GetContextName());
+        Log.std->debug("init: cef: context \"{}\"", GetContextName());
         InitializeSubContexts();
         InitializeContext();
     }

inexor/ui/layer/InexorAppLayer.cpp
@@ -31,7 +31,7 @@ void InexorAppLayer::Reload()
 void InexorAppLayer::Resize(int x, int y, int width, int height)
 {
     if (is_visible && layer.get()) {
-        Log.default->info("Resize APP layer: ({0}x{1} at {2}, {3})", width, height, x, y);
+        Log.std->info("Resize APP layer: ({0}x{1} at {2}, {3})", width, height, x, y);
         if (layer->GetInexorRenderHandler()->SetViewRect(x, y, width, height)) {
             layer->GetBrowser()->GetHost()->WasResized();
         }

inexorgame-obsolete/deprecated-cube-engine-inexor

inexor/ui/InexorRenderHandler.cpp
@@ -44,7 +44,7 @@ void InexorRenderHandler::Initialize()
     if (initialized)
         return;
 
-    Log.default->debug("InexorRenderHandler initializing...");
+    Log.ui->debug("InexorRenderHandler initializing...");
 
     // glHint(GL_POLYGON_SMOOTH_HINT, GL_NICEST); VERIFY_NO_ERROR;
 
@@ -67,7 +67,7 @@ void InexorRenderHandler::Initialize()
 
     setlocale(LC_ALL, "en_US.utf8");
 
-    Log.default->debug("InexorRenderHandler initialized!");
+    Log.ui->debug("InexorRenderHandler initialized!");
 
     initialized = true;
 }
@@ -125,17 +125,17 @@ void InexorRenderHandler::ClearPopupRects()
 bool InexorRenderHandler::GetViewRect(CefRefPtr<CefBrowser> browser, CefRect &rect)
 {
     rect = CefRect(view_x, view_y, view_width, view_height);
-    Log.default->debug("GetViewRect: ({0}x{1} at {2}, {3})", view_width, view_height, view_x, view_y);
+    Log.ui->debug("GetViewRect: ({0}x{1} at {2}, {3})", view_width, view_height, view_x, view_y);
     return true;
 }
 
 bool InexorRenderHandler::SetViewRect(int view_x, int view_y, int view_width, int view_height)
 {
     if (initialized) {
-        // Log.default->info() << "SetViewRect: (" << view_x << ", " << view_y << ", " << view_width << ", " << view_height << ")\n";
+        // Log.ui->info() << "SetViewRect: (" << view_x << ", " << view_y << ", " << view_width << ", " << view_height << ")\n";
         int view_width2 = (view_width / 32) * 32;
         int view_height2 = (view_height / 32) * 32;
-        Log.default->debug("SetViewRect: ({0}x{1} at {2}, {3})", view_width2, view_height2, view_x, view_y);
+        Log.ui->debug("SetViewRect: ({0}x{1} at {2}, {3})", view_width2, view_height2, view_x, view_y);
 
         bool success = this->view_x != view_x || this->view_y != view_y || this->view_width != view_width2 || this->view_height != view_height2;
         this->view_x = view_x;
@@ -145,7 +145,7 @@ bool InexorRenderHandler::SetViewRect(int view_x, int view_y, int view_width, in
         this->texture_initialized = false;
         return success;
     } else {
-        Log.default->debug("SetViewRect DEFERRED: ({0}x{1} at {2}, {3})", view_width, view_height, view_x, view_y);
+        Log.ui->debug("SetViewRect DEFERRED: ({0}x{1} at {2}, {3})", view_width, view_height, view_x, view_y);
 
         this->_view_x = view_x;
         this->_view_y = view_y;
@@ -164,14 +164,14 @@ void InexorRenderHandler::OnPaint(
 ) {
     if (!initialized) {
         if (CefCurrentlyOn(TID_UI)) {
-            Log.default->debug("OnPaint:Initialize ({}x{})", width, height);
-            Log.default->debug("OnPaint:BeforeInitialization ({0}x{1} at {2}, {3})", view_width, view_height, view_x, view_y);
+            Log.ui->debug("OnPaint:Initialize ({}x{})", width, height);
+            Log.ui->debug("OnPaint:BeforeInitialization ({0}x{1} at {2}, {3})", view_width, view_height, view_x, view_y);
             Initialize();
             browser->GetHost()->WasResized();
-            Log.default->debug("OnPaint:AfterInitialization ({0}x{1} at {2}, {3})", view_width, view_height, view_x, view_y);
+            Log.ui->debug("OnPaint:AfterInitialization ({0}x{1} at {2}, {3})", view_width, view_height, view_x, view_y);
             return;
         } else {
-            Log.default->debug("InexorRenderHandler::OnPaint() Wrong thread!");
+            Log.ui->debug("InexorRenderHandler::OnPaint() Wrong thread!");
             return;
         }
     }
@@ -200,12 +200,12 @@ void InexorRenderHandler::OnPaint(
         int old_height = view_height;
         // view_width = width;
         // view_height = height;
-        Log.default->debug("OnPaint:X ({}x{})", width, height);
+        Log.ui->debug("OnPaint:X ({}x{})", width, height);
         glPixelStorei(GL_UNPACK_ROW_LENGTH, view_width); VERIFY_NO_ERROR;
         // if (!texture_initialized || old_width != view_width || old_height != view_height || (dirtyRects.size() == 1 && dirtyRects[0] == CefRect(0, 0, view_width, view_height))) {
         if (!texture_initialized || old_width != view_width || old_height != view_height) {
             // Update/resize the whole texture.
-            Log.default->debug("OnPaint: Full Resize ({}x{})", width, height);
+            Log.ui->debug("OnPaint: Full Resize ({}x{})", width, height);
         	glPixelStorei(GL_UNPACK_SKIP_PIXELS, 0); VERIFY_NO_ERROR;
         	glPixelStorei(GL_UNPACK_SKIP_ROWS, 0); VERIFY_NO_ERROR;
         	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, view_width, view_height, 0, GL_BGRA, GL_UNSIGNED_INT_8_8_8_8_REV, buffer); VERIFY_NO_ERROR;
@@ -228,7 +228,7 @@ void InexorRenderHandler::OnPaint(
         int skip_rows = 0, y = popup_rect.y;
         int w = width;
         int h = height;
-        Log.default->debug("OnPaint:Y ({}x{})", width, height);
+        Log.ui->debug("OnPaint:Y ({}x{})", width, height);
 
         // Adjust the popup to fit inside the view.
         if (x < 0) {

inexor/ui/InexorSettings.hpp
@@ -41,7 +41,7 @@ class InexorSettings : public CefSettings
         CefString(&resources_dir_path).FromString(executable_path);
         CefString(&locales_dir_path).FromString(executable_path + "/locales");
         CefString(&log_file).FromString(std::string(base_path) + "inexorcef.log"); // TODO: make adaptable.
-        Log.default->info("init: cef: setting subprocess executable_path: {}", executable_path);
+        Log.ui->info("init: cef: setting subprocess executable_path: {}", executable_path);
         CefString(&browser_subprocess_path).FromString(executable_path + "/cef_subprocess.exe");
     }
 

inexor/ui/context/InexorContextProvider.cpp
@@ -11,7 +11,7 @@ CefRefPtr<CefV8Value> InexorContextProvider::GetContext()
     if (!context.get())
     {
         // This has to be happen when the context is initialized
-        Log.default->debug("init: cef: context \"{}\"", GetContextName());
+        Log.ui->debug("init: cef: context \"{}\"", GetContextName());
         InitializeSubContexts();
         InitializeContext();
     }

inexor/ui/layer/InexorAppLayer.cpp
@@ -31,7 +31,7 @@ void InexorAppLayer::Reload()
 void InexorAppLayer::Resize(int x, int y, int width, int height)
 {
     if (is_visible && layer.get()) {
-        Log.default->debug("Resize APP layer: ({0}x{1} at {2}, {3})", width, height, x, y);
+        Log.ui->debug("Resize APP layer: ({0}x{1} at {2}, {3})", width, height, x, y);
         if (layer->GetInexorRenderHandler()->SetViewRect(x, y, width, height)) {
             layer->GetBrowser()->GetHost()->WasResized();
         }