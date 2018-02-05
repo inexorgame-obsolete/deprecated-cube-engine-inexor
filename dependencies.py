524ac305bcb556ed4a26dfca8c8d22eafd68ad1f
Croydon <cr0ydon@outlook.com>
2018-02-06 00:19:11 +0100

    feat(ci): macOS: Specify compiler + compiler version
      * fix AppVeyor
      * override OpenSSL dep from gRPC

--- dependencies.py
+++ dependencies.py
@@ -1,18 +1,19 @@
 requires = (
     ("InexorGlueGen/0.6.6@inexorgame/stable"),
-    ("Protobuf/3.5.1@inexorgame/stable"),
+    ("Protobuf/3.5.1@inexorgame/stable"),  # todo: switch to bincrafters when stable
     ("gRPC/1.1.0@inexorgame/stable"),
+        ("OpenSSL/1.1.0g@conan/stable"),  # remove at next gRPC update
     ("doxygen/1.8.13@inexorgame/stable"),
     ("Boost/1.66.0@conan/stable"),
     ("zlib/1.2.11@conan/stable"),
     ("gtest/1.8.0@lasote/stable"),
     ("ENet/1.3.13@inexorgame/stable"),
     ("spdlog/0.14.0@bincrafters/stable"),
-    ("fmt/4.1.0@bincrafters/stable"),
+        ("fmt/4.1.0@bincrafters/stable"),  # fix version to make it re-producable
     ("SDL2/2.0.5@lasote/testing"),  # not self-contained
     ("SDL2_image/2.0.1@inexorgame/stable"),  # todo: unfork
-    ("libpng/1.6.34@bincrafters/stable"),  # override SDl2_image dep for Conan >= 0.30.0 compatibility
-    ("libjpeg-turbo/1.5.2@bincrafters/stable"),  # override SDl2_image dep for Conan >= 0.30.0 compatibility
+        ("libpng/1.6.34@bincrafters/stable"),  # override for Conan >= 0.30.0 compatibility
+        ("libjpeg-turbo/1.5.2@bincrafters/stable"),  # override for Conan >= 0.30.0 compatibility
     ("CEF/3.3239.1709.g093cae4@inexorgame/testing")  # not self-contained
 )
 
