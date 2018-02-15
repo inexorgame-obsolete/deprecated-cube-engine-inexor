7e548605d6adb99f58d6f6ad8d9d02713f2054ff
Croydon <cr0ydon@outlook.com>
2018-02-15 18:07:33 +0100

    update(dependencies): Update spdlog to 0.16.3

--- dependencies.py
+++ dependencies.py
@@ -8,7 +8,7 @@ requires = (
     ("zlib/1.2.11@conan/stable"),
     ("gtest/1.8.0@lasote/stable"),
     ("ENet/1.3.13@inexorgame/stable"),
-    ("spdlog/0.14.0@bincrafters/stable"),
+    ("spdlog/0.16.3@bincrafters/stable"),
         ("fmt/4.1.0@bincrafters/stable"),  # fix version to make it re-producable
     ("SDL2/2.0.5@lasote/testing"),  # not self-contained
     ("SDL2_image/2.0.1@inexorgame/stable"),  # todo: unfork
