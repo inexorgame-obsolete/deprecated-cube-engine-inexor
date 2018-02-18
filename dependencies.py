ab49a96ad2b7fa63e28cd2fd11761298f3ed433c
Croydon <cr0ydon@outlook.com>
2018-02-18 05:30:33 +0100

    update(dependencies): require at least CMake 3.9.2
    This is neccessary to find the Boost libs successfully

--- dependencies.py
+++ dependencies.py
@@ -9,7 +9,7 @@ requires = (
     ("gtest/1.8.0@lasote/stable"),
     ("ENet/1.3.13@inexorgame/stable"),
     ("spdlog/0.16.3@bincrafters/stable"),
-        ("fmt/4.1.0@bincrafters/stable"),  # fix version to make it re-producable
+        ("fmt/4.1.0@bincrafters/stable"),  # fix version to make it re-producable, remove at next spdlog update
     ("SDL2/2.0.5@lasote/testing"),  # not self-contained
     ("SDL2_image/2.0.1@inexorgame/stable"),  # todo: unfork
         ("libpng/1.6.34@bincrafters/stable"),  # override for Conan >= 0.30.0 compatibility
