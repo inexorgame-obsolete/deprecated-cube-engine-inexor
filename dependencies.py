819169a4440c4bbb8abf2dc5e293b92a678f4f5b
Croydon <cr0ydon@outlook.com>
2018-04-14 17:51:55 +0200

    fix(build): Conan is confused by the lower/uppercase mix of SDL2 packages

--- dependencies.py
+++ dependencies.py
@@ -10,7 +10,7 @@ requires = (
     ("spdlog/0.16.3@bincrafters/stable"),
         ("fmt/4.1.0@bincrafters/stable"),  # fix version to make it re-producable, remove at next spdlog update
     ("sdl2/2.0.8@bincrafters/testing"),  # not self-contained
-    ("SDL2_image/2.0.1@inexorgame/stable"),  # todo: unfork
+    ("sdl2_image_lowercase/2.0.1@inexorgame/stable"),  # todo: unfork
         ("libpng/1.6.34@bincrafters/stable"),  # override for Conan >= 0.30.0 compatibility
         ("libjpeg-turbo/1.5.2@bincrafters/stable"),  # override for Conan >= 0.30.0 compatibility
     ("CEF/3.3239.1709.g093cae4@inexorgame/testing")  # not self-contained
