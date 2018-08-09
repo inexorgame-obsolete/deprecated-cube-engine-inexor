38c234a634480f3e036b9d3844645dae4aba32ac
Croydon <cr0ydon@outlook.com>
2018-08-09 23:08:56 +0200

    chore(ci): Create seperate file for Conan upload logic
    and share this logic between Linux and macOS

--- tool/travis.sh
+++ tool/travis.sh
@@ -131,11 +131,7 @@ EOF
 # Upload nightly
 upload_nightlies() {
   if test "$NIGHTLY" = conan; then
-    # Upload all conan packages to our Bintray repository
-    conan user -p "${NIGHTLY_PASSWORD}" -r inexor "${NIGHTLY_USER}"
-    set -f
-    conan upload --all --force -r inexor --retry 3 --retry-wait 10 --confirm "*stable*"
-    set +f
+    source ./travis_conan_upload.sh
   fi
 }
 
